9b35302791c563233ac506a5ae43b159e1d9e4f5
jony wu <37248488+jonyworker@users.noreply.github.com>
2024-09-14 18:45:15 +0800

    ✨feat：新增 Breadcrumb 元件。

---
+++ libs/src/ui/element/Breadcrumb/Breadcrumb.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { computed, ref } from "vue";
+import BreadcrumbItem from "@/ui/element/Breadcrumb/BreadcrumbItem.vue";
+
+// 定義 Props
+const props = defineProps({
+	breadcrumbData: {
+		type: Array,
+		required: true,
+		default: () => []
+	},
+	// --  資料接口 -- //
+	title: {
+		type: String,
+	},
+	href: {
+		type: String,
+	},
+});
+const copyBreadcrumbsData = ref([...props.breadcrumbData])
+console.log(copyBreadcrumbsData.value.length)
+
+const truncatedBreadcrumbs = computed(() => {
+	const data = copyBreadcrumbsData.value;
+	if (data.length > 3) {
+		return [data[0], { label: '...', href: '#' }, ...data.slice(-2)];
+	} else {
+		return data;
+	}
+});
+
+const restBreadcrumbs = computed(() => {
+	const data = copyBreadcrumbsData.value;
+	if (data.length > 3) {
+		return data.slice(1, -2);
+	} else {
+		return [];
+	}
+});
+
+console.log("1st",truncatedBreadcrumbs.value)
+console.log("2nd",restBreadcrumbs.value)
+
+// 控制顯示剩餘未顯示數據
+const isOpen =ref(false)
+const handleClick = () => {
+	isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+	<nav class="breadcrumb-container undefined">
+		<ul class="breadcrumb">
+			<li class="breadcrumb-item" v-for="(item, index) in truncatedBreadcrumbs">
+				<!-- breadcrumb - 等於...時折疊數據 -->
+				<template v-if="item.label === '...'">
+					<div class="rest">
+						<span class="rest-label" @click="handleClick()">...</span>
+						<ul class="rest-dropdown-menu" v-if="isOpen">
+							<li class="drop-item" v-for="(restItem) in restBreadcrumbs">
+								<a class="drop-item-link" :href="restItem.href">{{ restItem.label }}</a>
+							</li>
+						</ul>
+					</div>
+				</template>
+				<!-- breadcrumb - 其餘非折疊數據 -->
+				<template v-else>
+					<BreadcrumbItem
+						:label="item.label"
+						:href="item.href"
+						:isCurrentPage="index === truncatedBreadcrumbs.length - 1">
+					</BreadcrumbItem>
+				</template>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<style scoped lang="scss">
+
+</style>
