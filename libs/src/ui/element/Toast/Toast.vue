b4030a8f3d5514490b58d8a2cfff14c0a7ae6480
jonyworker <jonywu@gmail.com>
2024-10-06 03:56:06 +0800

    🆙update：Toast.stories.js, Toast.vue 和 useToast.js。
    [Toast.stories.js]
    - 調整屬性順序
    - 隱藏不必要 control
    - 增加基礎樣式 storybook 用關閉功能
    - storybook control 功能測試 ok
    
    [Toast.vue]
    - 調整屬性順序
    - 重構部分程式碼
     - 調整關閉邏輯，由元純外部控制改為內部duration emit 外部控制
     - 刪除 themeColor, show, id 屬性
    - 取消 content slot 改由 props 傳值
    
    [useToast.js]
    - 取消由 useToast 控制 Toast 顯示時間

--- libs/src/ui/element/Toast/Toast.vue
+++ libs/src/ui/element/Toast/Toast.vue
@@ -1,25 +1,17 @@
 <script setup>
-import {ref, onUnmounted, watch} from 'vue'
+import {ref, onMounted, onUnmounted } from 'vue'
 import Icon from "@/ui/element/Icon/Icon.vue";
 
 // 定義 Emits
-const emits = defineEmits(['close'])
+const emit = defineEmits(['close'])
 
 // 定義 Props
 const props = defineProps({
-	themeColor: {
+	title: {
 		type: String,
-		default: 'primary',
-		validator: (value) =>
-			[
-				'primary',
-				'secondary',
-				'tertiary',
-				'success',
-				'warning',
-				'error',
-				'info',
-			].includes(value),
+	},
+	message: {
+		type: String,
 	},
 	severity:{
 		type: String,
@@ -27,48 +19,36 @@ const props = defineProps({
 		validator: (value) =>
 			['success', 'warning', 'error', 'info'].includes(value),
 	},
-	title: {
-		type: String,
-		required: true
-	},
-	message: {
-		type: String,
-		required: true
-	},
 	duration: {
 		type: Number,
-		default: 3000
+		default: 3000,
 	},
-	id:{
-		type: String,
-	},
-	show: {
-		type: Boolean,
-		default: false
-	}
 })
 // 控制 關閉
 const handleClose = () => {
-	emits('close');
+	emit('close');
 };
 
 // 控制 自動關閉
-const isVisible = ref(props.show)
 let timer = ref(null)
 const show = () => {
-	isVisible.value = true
-	timer = setTimeout(() => {
-		isVisible.value = false
-		emits('update:show')
-	}, 3000)
+	if (timer.value) clearTimeout(timer.value);
+	timer.value = setTimeout(() => {
+		handleClose()
+	}, props.duration);
 }
 
 // 有新值則設定新的關閉時間
-watch(() => props.show, (newVal) => {
-	if (newVal) show();
+// watch(() => props.trigger, (newVal) => {
+// 	if (newVal) show();
+// });
+
+// 元件掛載時 setTimeout
+onMounted(() => {
+	show();
 });
 
-// 卸載 setTimeout
+// 元件卸載時卸載 setTimeout
 onUnmounted(() => {
 	if (timer) clearTimeout(timer)
 })
@@ -76,16 +56,16 @@ onUnmounted(() => {
 
 <template>
 	<teleport to="#toast">
-		<div class="toast" :class="`toast-border-${props.themeColor}`">
+		<div class="toast" :class="`toast-border-${props.severity}`">
 			<!-- toast - 關閉按鈕 -->
-			<button class="close-button" @click="handleClose">
+			<button class="close-button" style="cursor: pointer" @click="handleClose">
 				<Icon name="close" size="20"></Icon>
 			</button>
 			<!-- toast - 標題及說明文字 -->
 			<p class="message">
-			    <span class="icon-wrapper" :class="`toast-${props.themeColor}`">
+			    <span class="icon-wrapper" :class="`toast-${props.severity}`">
 				    <template v-if="props.severity ==='success'">
-					    <Icon  name="check" size="14"></Icon>
+					    <Icon name="check" size="14"></Icon>
 				    </template>
 				    <template v-if="props.severity ==='error'">
 					    <Icon name="close" size="14"></Icon>
