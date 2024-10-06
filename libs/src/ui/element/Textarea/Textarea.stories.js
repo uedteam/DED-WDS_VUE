fb4113de2926dfaf8fc730b8354a903fbde0fb8a
jonyworker <jonywu@gmail.com>
2024-10-06 22:18:39 +0800

    🆙update：Textarea.stories.js, Textarea.vue
    [Textarea.stories.js]
    - 調整屬性順序
    - 隱藏不必要 control
    - storybook control 功能測試 ok
    
    [Textarea.vue]
    - 調整屬性順序

--- libs/src/ui/element/Textarea/Textarea.stories.js
+++ libs/src/ui/element/Textarea/Textarea.stories.js
@@ -8,35 +8,34 @@ export default {
     tags: ["autodocs"],
     argTypes: {
         label: {
-            description: '設定 Textarea 標題',
+            description: '設定 Textarea 的標題文字',
             control: { type: 'text' },
         },
         placeholder: {
-            description: '設定 Textarea 輸入提示',
+            description: '設定 Textarea 的輸入提示文字，指引用戶輸入內容',
             control: { type: 'text' },
         },
         total: {
-            description: '設定 Textarea 標題',
+            description: '設定 Textarea 的最大字符數限制',
             control: { type: 'number' },
-            default: 10,
+        },
+        isDisabled: {
+            description: '設定 Textarea 是否禁用，禁用後無法進行任何輸入',
+            control: { type: 'boolean' },
         },
         hint: {
-            description: 'Textarea 提示訊息',
+            description: '顯示在 Textarea 下方的提示訊息，提供額外指引',
             control: {
                 type: 'object',
             },
-        },
-        isDisable: {
-            description: '是否禁用',
-            control: { type: 'boolean' },
         },
     },
     parameters: {
         // 自動文件
         docs: {
-            title: "多行文字輸入框",
+            title: "Textarea",
             description: {
-                component: "多行文字輸入框組件的呈現及說明。",
+                component: "Textarea 組件的呈現及說明。",
             },
         },
     },
@@ -45,20 +44,19 @@ export default {
     // args: { onClick: fn() },
 };
 
-//==== 基礎多行文字輸入框 ====//
-export const DefaultTextarea = {
-    name: "基礎多行文字輸入框",
+//==== Textarea 基礎樣式 ====//
+export const TextareaDefault = {
+    name: "Textarea 基礎樣式",
     args: {
         label: "訂單備註",
         placeholder: "請輸入此筆訂單備註",
         total: 50,
-        hint: { error: '', description: '' },
         isDisabled: false,
+        hint: { error: '', description: '' },
     },
     render: (args) => ({
         components: { Textarea },
         setup() {
-            // Create a ref for modelValue to be used with v-model
             const textareaModelValue =ref("");
             return {
                 args,
@@ -67,12 +65,12 @@ export const DefaultTextarea = {
         },
         template: `
             <Textarea
-              :label="args.label"
-              :placeholder="args.placeholder"
-              :total="args.total"
-              :hint="args.hint"
-              v-model="textareaModelValue"
-              :isDisabled="args.isDisabled"
+                :label="args.label"
+                :placeholder="args.placeholder"
+                :total="args.total"
+                :isDisabled="args.isDisabled"
+                :hint="args.hint"
+                v-model="textareaModelValue"
             ></Textarea>
         `,
     }),
@@ -80,13 +78,14 @@ export const DefaultTextarea = {
     parameters: {
         controls: {
             // include: ['themeColor', 'label', 'value', 'name' ],
+            exclude: ['modelValue', 'customClass'],
         },
     },
 };
 
-//==== 多行文字輸入框驗證狀態 ====//
+//==== Textarea 驗證樣式 ====//
 export const TextareaStatus = {
-    name: "多行文字輸入框驗證狀態",
+    name: "Textarea 驗證樣式",
     args: {
         label: "訂單備註",
         placeholder: "請輸入此筆訂單備註",
@@ -108,38 +107,41 @@ export const TextareaStatus = {
             };
         },
         template: `
-          <div style="display: flex; gap:16px">
-            <Textarea
-                :label="args.label"
-                :placeholder="args.placeholder"
-                :total="args.total"
-                :hint="{ error: '', description: '' }"
-                v-model="textareaModelValue01"
-                :isDisabled="args.isDisabled"
-            ></Textarea>
-            <Textarea
-                :label="args.label"
-                :placeholder="args.placeholder"
-                :total="args.total"
-                :hint="{ error: '請勿使用特殊符號', description: '請簡述本筆訂單的使用回饋' }"
-                v-model="textareaModelValue02"
-                :isDisabled="args.isDisabled"
-            ></Textarea>
-            <Textarea
-                :label="args.label"
-                :placeholder="args.placeholder"
-                :total="args.total"
-                :hint="{ error: '', description: '請簡述本筆訂單的使用回饋' }"
-                v-model="textareaModelValue03"
-                :isDisabled="args.isDisabled"
-            ></Textarea>
-          </div>
+            <div style="display: flex; gap:16px">
+                <Textarea
+                    :label="args.label"
+                    :placeholder="args.placeholder"
+                    :total="args.total"
+                    :isDisabled="args.isDisabled"
+                    :hint="{ error: '', description: '' }"
+                    v-model="textareaModelValue01"
+                ></Textarea>
+                
+                <Textarea
+                    :label="args.label"
+                    :placeholder="args.placeholder"
+                    :total="args.total"
+                    :isDisabled="args.isDisabled"
+                    :hint="{ error: '請勿使用特殊符號', description: '' }"
+                    v-model="textareaModelValue02"
+                ></Textarea>
+                
+                <Textarea
+                    :label="args.label"
+                    :placeholder="args.placeholder"
+                    :total="args.total"
+                    :isDisabled="args.isDisabled"
+                    :hint="{ error: '', description: '請簡述本筆訂單的使用回饋' }"
+                    v-model="textareaModelValue03"
+                ></Textarea>
+            </div>
         `,
     }),
     // 控制 controls 中能控制的參數
     parameters: {
         controls: {
             // include: ['themeColor', 'label', 'value', 'name' ],
+            exclude: ['modelValue', 'hint', 'customClass'],
         },
     },
 };
