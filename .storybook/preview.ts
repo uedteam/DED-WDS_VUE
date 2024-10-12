31ffbac783533b41df7c60b4812a043dd2d44bb6
jonyworker <jonywu@gmail.com>
2024-10-12 20:33:34 +0800

    🆙update：preview 創建 toastContainer, dialogContainer 容器，解決彈窗無法顯示於最上層問題。

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,16 @@ import { Preview } from '@storybook/vue3';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 /* snipped for brevity */
+// 創建 Toast 容器
+const toastContainer = document.createElement('div');
+toastContainer.id = 'toast';
+toastContainer.className = 'toast-container';
+document.body.appendChild(toastContainer);
+
+// 創建 Dialog 容器
+const dialogContainer = document.createElement('div');
+dialogContainer.id = 'dialog';
+document.body.appendChild(dialogContainer);
 
 const preview: Preview = {
   parameters: {
@@ -16,17 +26,17 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-    docs: {
-      toc: {
-        contentsSelector: '.sbdocs-content',
-        headingSelector: 'h2, h3',
-        ignoreSelector: '#primary',
-        disable: false,
-        unsafeTocbotOptions: {
-          orderedList: false,
-        },
-      }, // 👈 Enables the table of contents
-    },
+    // docs: {
+    //   toc: {
+    //     contentsSelector: '.sbdocs-content',
+    //     headingSelector: 'h2, h3',
+    //     ignoreSelector: '#primary',
+    //     disable: false,
+    //     unsafeTocbotOptions: {
+    //       orderedList: false,
+    //     },
+    //   }, // 👈 Enables the table of contents
+    // },
     options: {
       storySort: {
         method: 'alphabetical', // 可選擇 'alphabetical', 'configure', 或其他自定義方法
