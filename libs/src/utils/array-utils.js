853602dcecdfccc7d30c1d6eff922c9113e87609
jonyworker <jonywu@gmail.com>
2024-10-17 22:01:02 +0800

    🆙update：將 function 合併到各自 vue 內。

--- libs/src/utils/array-utils.js
+++
@@ -1,6 +0,0 @@
-export const splitArrayAt = (arr, splitCount) => {
-    const currList = arr.slice(0, splitCount);
-    const restList = arr.slice(splitCount);
-
-    return { currList, restList };
-};
