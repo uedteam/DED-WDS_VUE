014759027977fa40beb5172e000978871f994ac2
jonyworker <jonywu@gmail.com>
2024-10-19 23:41:23 +0800

    ✨feat：新增 ListItem 元件。 - 新增 _list.scss - 新增 ListItem.stories.js - 新增 ListItem.vue

---
+++ libs/src/ui/element/ListItem/ListItem.vue
@@ -0,0 +1,52 @@
+<script setup>
+// 定義 Props
+const props = defineProps({
+	label: {
+		type: String,
+		default: "標題一"
+	},
+	value: {
+		type: String,
+		default: "value1"
+	},
+	href: {
+		type: String,
+	},
+	className: {
+		type: String,
+	},
+})
+
+// 新頁面開起網頁
+const handleClick = () => {
+	if (!props.href) {
+		return;
+	}else {
+		window.open(props.href, '_blank');
+	}
+	props.onClick && props.onClick(props.value);
+};
+</script>
+
+<template>
+	<div
+		:class="{
+			item: true,
+			'menu-link': props.href,
+			[props.className]: !!props.className
+		}"
+		@click="handleClick">
+
+		<slot name="listPrefix">
+		</slot>
+
+		<div class="item-label">{{props.label}}</div>
+
+		<slot name="listSuffix">
+		</slot>
+	</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
