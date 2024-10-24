0c17446b3fedbf5c3f19cb170899ee6c270543c8
jonyworker <jonywu@gmail.com>
2024-10-24 23:28:07 +0800

    ✨feat：新增 dropdown 元件

---
+++ libs/src/ui/element/Dropdown/Dropdown.vue
@@ -0,0 +1,89 @@
+<script setup>
+import { ref } from 'vue'
+import Icon from '@/ui/element/Icon/Icon.vue'
+import ListItem from "@/ui/element/ListItem/ListItem.vue";
+import List from "@/ui/element/List/List.vue";
+
+// 定義 Model (改成最新寫法)
+const modelValue = defineModel()
+
+// 定義 Props
+//TODO: prop還沒確認
+const props = defineProps({
+	label: {
+		type: String,
+	},
+	placeholder: {
+		type: String,
+	},
+	size: {
+		type: String,
+		validator: (value) => ['small', 'medium', 'large'].includes(value),
+		default: 'medium',
+	},
+	maxHeight: {
+		type: String,
+	},
+	className: {
+		type: String,
+		default: '',
+	},
+	options: {
+		type: Array,
+		default: () => [],
+	}
+})
+
+
+const selectValue =  ref(null)
+
+const isDropdownVisible = ref(false) //控制下拉面板顯示
+
+const handleSelect = (value) => {
+	selectValue.value = value
+	isDropdownVisible.value = !isDropdownVisible
+}
+</script>
+
+<template>
+
+	<div class="dropdown-container">
+		<!-- 輸入框標題 -->
+		<label v-if="props.label" class="input-label">{{props.label}}</label>
+		<div ref="dropdown"
+		     :class="['dropdown', `dropdown-${props.size}`]"
+		     role="listbox"
+		     tabindex="0"
+		     @click="isDropdownVisible = !isDropdownVisible">
+				<span :class="selectValue ? 'dropdown-selectValue-select' : 'dropdown-selectValue-unselect'">
+					{{ selectValue || props.placeholder }}
+				</span>
+
+				<Icon :class="`icon-${size}`"  style="margin-left: auto;" name="arrow_down"></Icon>
+		</div>
+
+		<List
+			className=""
+			:isMenu=true
+			:maxHeight="props.maxHeight"
+			v-if="isDropdownVisible"
+		>
+			<ListItem
+				v-for="(item, index) in props.options"
+				:key="index"
+				:label="item.label"
+				:value="item.value"
+				:href="item.href"
+				@selectedItem="(value) => handleSelect(value)"
+			>
+
+			</ListItem>
+		</List>
+	</div>
+
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
