88d7d0299f2a50d85ea1ed7c4ef48d12d690fab4
jonyworker <jonywu@gmail.com>
2024-10-31 02:28:52 +0800

    🆙update：修改 Divider 元件
    - 改寫 scss

--- libs/src/ui/element/Divider/Divider.stories.js
+++ libs/src/ui/element/Divider/Divider.stories.js
@@ -2,138 +2,144 @@ import Divider from "./Divider.vue";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
-	title: "Design System/Divider",
-	component: Divider,
-	tags: ["autodocs"],
-	argTypes: {
-		//以下參數不刪
-		layout: {
-			description: "設定 Divider  方向",
-			control: { type: "radio" },
-			defaultValue: "horizontal",
-			options: ["horizontal", "vertical"],
-		},
-		width: {
-			description: "設定 Divider  寬度",
-			control: { type: "select" },
-			options: ["xsmall (1px)", "small (2px)", "medium (4px)", "large (6px)", "xlarge (8px)"],
-			mapping: {
-				'xsmall (1px)': 'xsmall',
-				'small (2px)': 'xsmall',
-				'medium (4px)': 'medium',
-				'large (6px)': 'large',
-				'xlarge (8px)': 'xlarge',
-			},
-		},
-		type: {
-			description: "設定 Divider  種類",
-			control: { type: "select" },
-			defaultValue: "solid",
-			options: ["solid", "dashed", "dotted"],
-		},
-		themeColor: {
-			description: "設定 Divider 主題顏色",
-			control: { type: "select" },
-			options: [
-				"None",
-				"primary",
-				"secondary",
-				"tertiary",
-				"success",
-				"warning",
-				"error",
-				"info",
-			],
-			mapping: {
-				'None': null
-			},
-		},
-		align: {
-			description: "設定 Divider 上文字位置",
-			control: { type: "radio" },
-			options: ["start", "center", "end"],
-		},
+    title: "Design System/Divider",
+    component: Divider,
+    tags: ["autodocs"],
+    argTypes: {
+        //以下參數不刪
+        layout: {
+            description: "設定 Divider  方向",
+            control: { type: "radio" },
+            defaultValue: "horizontal",
+            options: ["horizontal", "vertical"],
+        },
+        width: {
+            description: "設定 Divider  寬度",
+            control: { type: "select" },
+            options: [
+                "xsmall (1px)",
+                "small (2px)",
+                "medium (4px)",
+                "large (6px)",
+                "xlarge (8px)",
+            ],
+            mapping: {
+                "xsmall (1px)": "xsmall",
+                "small (2px)": "xsmall",
+                "medium (4px)": "medium",
+                "large (6px)": "large",
+                "xlarge (8px)": "xlarge",
+            },
+        },
+        type: {
+            description: "設定 Divider  種類",
+            control: { type: "select" },
+            defaultValue: "solid",
+            options: ["solid", "dashed", "dotted"],
+        },
+        themeColor: {
+            description: "設定 Divider 主題顏色",
+            control: { type: "select" },
+            options: [
+                "None",
+                "primary",
+                "secondary",
+                "tertiary",
+                "success",
+                "warning",
+                "error",
+                "info",
+            ],
+            mapping: {
+                None: null,
+            },
+        },
+        align: {
+            description: "設定 Divider 上文字位置",
+            control: { type: "radio" },
+            options: ["start", "center", "end"],
+        },
+    },
+    parameters: {
+        // 自動文件
+        docs: {
+            title: "Divider",
+            description: {
+                component: "Divider 組件的呈現及說明。",
+            },
+        },
+        slots: {
+            default: {
+                description: "接收 Divider 文字",
+                template: `{{ args.default }}`,
+            },
+        },
+    },
 
-	},
-	parameters: {
-		// 自動文件
-		docs: {
-			title: "Divider",
-			description: {
-				component: "Divider 組件的呈現及說明。",
-			},
-		},
-		slots: {
-			default: {
-				description: '接收 Divider 文字',
-				template: `{{ args.default }}`,
-			},
-		},
-	},
-
-	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-	// args: { onClick: fn() },
+    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+    // args: { onClick: fn() },
 };
 
 //==== Divider 基礎樣式 ====//
 export const DividerDefault = {
-	name: "Divider 基礎樣式",
-	args: {
-		default: 'Divider Text',
-		layout: "horizontal",
-		width: 'small',
-		type: 'solid',
-		themeColor: "None",
-		align: 'start'
-	},
-	render: (args) => ({
-		components: { Divider },
-		setup() {
-			// Create a ref for modelValue to be used with v-model
-			return {
-				args,
-			};
-		},
-		template: `
-			<div style="display:flex; align-items:center; justify-content: center; gap: 16px; height: 200px">
+    name: "Divider 基礎樣式",
+    args: {
+        default: "Divider Text",
+        layout: "horizontal",
+        width: "small",
+        type: "solid",
+        themeColor: "None",
+        align: "start",
+    },
+    render: (args) => ({
+        components: { Divider },
+        setup() {
+            // Create a ref for modelValue to be used with v-model
+            return {
+                args,
+            };
+        },
+        template: `
+			<div style="display:flex; flex-direction: column; gap: 16px; height: 200px">
 				<Divider
 					:layout="args.layout"
 					:width="args.width"
 					:type="args.type"
 					:themeColor="args.themeColor"
 					:align="args.align"
+					style="flex-grow:1"
 				>{{args.default}}</Divider>
 			</div>
 		`,
-	}),
-	// 控制 controls 中能控制的參數
-	parameters: {
-		controls: {
-			// include: ['themeColor', 'label', 'value', 'name' ],
-		},
-	},
+    }),
+    // 控制 controls 中能控制的參數
+    parameters: {
+        controls: {
+            // include: ['themeColor', 'label', 'value', 'name' ],
+        },
+    },
 };
 
 //==== Divider 種類 ====//
 export const DividerTypes = {
-	name: "Divider 種類",
-	args: {
-		layout: "horizontal",
-		width: 'small',
-		themeColor: "None",
-		align: 'start',
-	},
-	render: (args) => ({
-		components: { Divider },
-		setup() {
-			// Create a ref for modelValue to be used with v-model
-			return {
-				args,
-			};
-		},
-		template: `
+    name: "Divider 種類",
+    args: {
+        layout: "horizontal",
+        width: "small",
+        themeColor: "None",
+        align: "start",
+    },
+    render: (args) => ({
+        components: { Divider },
+        setup() {
+            // Create a ref for modelValue to be used with v-model
+            return {
+                args,
+            };
+        },
+        template: `
 			<div :style="{ flexDirection: args.layout === 'horizontal' ? 'column' : 'row' }"
-			     style="display:flex; align-items:center; justify-content: center; gap: 16px; height: 200px">
+			     style="display:flex; gap: 16px; height: 200px">
 				<Divider
 					:layout="args.layout"
 					:width="args.width"
@@ -157,37 +163,37 @@ export const DividerTypes = {
 				>dotted</Divider>
 			</div>
 		`,
-	}),
-	// 控制 controls 中能控制的參數
-	parameters: {
-		controls: {
-			// include: ['themeColor', 'label', 'value', 'name' ],
-			exclude: ["type"],
-		},
-	},
+    }),
+    // 控制 controls 中能控制的參數
+    parameters: {
+        controls: {
+            // include: ['themeColor', 'label', 'value', 'name' ],
+            exclude: ["type"],
+        },
+    },
 };
 
 //==== Divider 粗細 ====//
 export const DividerWidth = {
-	name: "Divider 粗細",
-	args: {
-		layout: "horizontal",
-		width: 'small',
-		type: 'solid',
-		themeColor: "None",
-		align: 'start'
-	},
-	render: (args) => ({
-		components: { Divider },
-		setup() {
-			// Create a ref for modelValue to be used with v-model
-			return {
-				args,
-			};
-		},
-		template: `
+    name: "Divider 粗細",
+    args: {
+        layout: "horizontal",
+        width: "small",
+        type: "solid",
+        themeColor: "None",
+        align: "start",
+    },
+    render: (args) => ({
+        components: { Divider },
+        setup() {
+            // Create a ref for modelValue to be used with v-model
+            return {
+                args,
+            };
+        },
+        template: `
 			<div :style="{ flexDirection: args.layout === 'horizontal' ? 'column' : 'row' }"
-			     style="display:flex; align-items:center; justify-content: center; gap: 16px; height: 400px">
+					style="display:flex; gap: 16px; height: 400px">
 				<Divider
 					:layout="args.layout"
 					width="xsmall"
@@ -225,33 +231,33 @@ export const DividerWidth = {
 				>xlarge / 8px</Divider>
 			</div>
 		`,
-	}),
-	// 控制 controls 中能控制的參數
-	parameters: {
-		controls: {
-			// include: ['themeColor', 'label', 'value', 'name' ],
-			exclude: ["width"],
-		},
-	},
+    }),
+    // 控制 controls 中能控制的參數
+    parameters: {
+        controls: {
+            // include: ['themeColor', 'label', 'value', 'name' ],
+            exclude: ["width"],
+        },
+    },
 };
 
 //==== Divider 文字位置 ====//
 export const DividerContent = {
-	name: "Divider 文字位置",
-	args: {
-		width: 'small',
-		type: 'solid',
-		themeColor: "None",
-	},
-	render: (args) => ({
-		components: { Divider },
-		setup() {
-			// Create a ref for modelValue to be used with v-model
-			return {
-				args,
-			};
-		},
-		template: `
+    name: "Divider 文字位置",
+    args: {
+        width: "small",
+        type: "solid",
+        themeColor: "None",
+    },
+    render: (args) => ({
+        components: { Divider },
+        setup() {
+            // Create a ref for modelValue to be used with v-model
+            return {
+                args,
+            };
+        },
+        template: `
 			<div style="display:flex; flex-direction: column; gap: 16px">
 				<Divider
 					layout="horizontal"
@@ -300,37 +306,37 @@ export const DividerContent = {
 				>end</Divider>
 			</div>
 		`,
-	}),
-	// 控制 controls 中能控制的參數
-	parameters: {
-		controls: {
-			// include: ['themeColor', 'label', 'value', 'name' ],
-			exclude: ["layout", "align"],
-		},
-	},
+    }),
+    // 控制 controls 中能控制的參數
+    parameters: {
+        controls: {
+            // include: ['themeColor', 'label', 'value', 'name' ],
+            exclude: ["layout", "align"],
+        },
+    },
 };
 
 //==== Divider 顏色 ====//
 export const DividerColors = {
-	name: "Divider 顏色",
-	args: {
-		layout: "horizontal",
-		width: 'small',
-		type: 'solid',
-		themeColor: "None",
-		align: 'start'
-	},
-	render: (args) => ({
-		components: { Divider },
-		setup() {
-			// Create a ref for modelValue to be used with v-model
-			return {
-				args,
-			};
-		},
-		template: `
+    name: "Divider 顏色",
+    args: {
+        layout: "horizontal",
+        width: "small",
+        type: "solid",
+        themeColor: "None",
+        align: "start",
+    },
+    render: (args) => ({
+        components: { Divider },
+        setup() {
+            // Create a ref for modelValue to be used with v-model
+            return {
+                args,
+            };
+        },
+        template: `
 			<div :style="{ flexDirection: args.layout === 'horizontal' ? 'column' : 'row' }"
-			     style="display:flex; align-items:center; justify-content: center; gap: 16px; height: 600px">
+			     style="display:flex; gap: 16px; height: 400px">
 				<Divider
 					:layout="args.layout"
 					:width="args.width"
@@ -388,12 +394,12 @@ export const DividerColors = {
 				>info</Divider>
 			</div>
 		`,
-	}),
-	// 控制 controls 中能控制的參數
-	parameters: {
-		controls: {
-			// include: ['themeColor', 'label', 'value', 'name' ],
-			exclude: ["themeColor"],
-		},
-	},
+    }),
+    // 控制 controls 中能控制的參數
+    parameters: {
+        controls: {
+            // include: ['themeColor', 'label', 'value', 'name' ],
+            exclude: ["themeColor"],
+        },
+    },
 };
