aaba0f238f81397403ccb4d331d90fa3e0cd9077
jonyworker <jonywu@gmail.com>
2024-10-31 02:39:12 +0800

    ✨feat：新增 Menu 元件

--- libs/src/ui/element/Menu/Menu.vue
+++ libs/src/ui/element/Menu/Menu.vue
@@ -1,38 +1,104 @@
 <script setup>
-import Icon from "@/ui/element/Icon/Icon.vue";
+import Icon from "@/ui/element/Icon/Icon.vue"
+import { computed, defineEmits, ref } from "vue";
 
+const isExpandedModelValue = defineModel('isExpanded')
 // 定義 Props
 const props = defineProps({
-  // --  資料接口 -- //
-  menuData: {
-    type: Array,
-  },
-  // --  樣式接口 -- //
-  limit: {
-    type: Number,
-    default: 1,
-  },
-  customClass: {
-    type: String,
-    default: '',
-  },
+	// --  資料接口 -- //
+	menuData: {
+		type: Array,
+		required: true,
+		default: () => [],
+	},
+	activePath: {
+		type: String,
+		default: "",
+	},
+	customClass: {
+		type: String,
+		default: '',
+	},
+	fontColor: {
+		type: String,
+		default: '#000',
+	}
 })
 
+const emit = defineEmits(["navItemClick","expandedNav"]);
+
+// 追踪展開的子菜單
+const expandedItems = ref({});
+
+// 處理項目點擊
+const handleItemClick = (item) => {
+	emit("navItemClick", item); // 告知被點擊item
+	emit("expandedNav"); // 告知展開導航
+	if (item.children) {
+		// 切換子菜單的展開狀態
+		expandedItems.value[item.path] = !expandedItems.value[item.path];
+	}
+};
 </script>
 
 <template>
-<!--  TODO: Menu 還有細部沒做完-->
-  <ul :class="['menu', customClass]">
-    <template v-if="$slots.default">
-      <slot></slot>
-    </template>
-    <template v-else>
-      <li class="menu-item" v-for="(menu) in menuData">
-        <div style="margin-right: auto;">{{ menu.userName }}</div>
-        <Icon name="arrow_down" size="24" ></Icon>
-      </li>
-    </template>
-  </ul>
+	<!--改造成學長版-->
+	<div class="menu-container">
+		<nav class="nav">
+			<ul class="nav-menu">
+				<li v-for="item in menuData" :key="item.path">
+					<div class="menu-item" @click.stop="handleItemClick(item)" :style="`color:${props.fontColor}`">
+						<div class="menu-item-content" >
+							<template v-if="item.icon" >
+								<Icon class="menu-item-content-icon" :name="item.icon" size="20"></Icon>
+							</template>
+
+							<span class="menu-item-content-title"
+							      :style="isExpandedModelValue? 'opacity: 1; display: block' :
+							      'opacity:0; display: none'">{{item.label
+								}}</span>
+
+							<template v-if="isExpandedModelValue && item.children" >
+								<Icon
+									class="menu-item-content-icon"
+									:name="expandedItems[item.path]
+                                    ? 'chevronDown'
+                                    : 'chevronRight'"
+									size="16"
+								></Icon>
+							</template>
+
+						</div>
+					</div>
+
+					<!-- submenu -->
+					<ul class="menu-subitem" v-if="
+	                    isExpandedModelValue &&
+	                    item.children &&
+	                    expandedItems[item.path]
+	                ">
+						<li v-for="child in item.children"
+						    :key="child.path"
+						    @click.stop="handleItemClick(child)"
+						    >
+							<div class="menu-item">
+								<div class="menu-item-content" :style="`color:${props.fontColor}`">
+									<template v-if="item.icon">
+										<div class="menu-item-content-icon"  v-if="child.icon">
+											<Icon class="nav-item-icon" :name="child.icon" size="20"></Icon>
+										</div>
+									</template>
+
+									<span class="menu-item-content-title" style="opacity: 1">{{ child.label }}</span>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+	</div>
+
 </template>
 
 <style scoped lang="scss">
