b27cbe10f0c65af6431e3a871c1b8084c1076860
jony wu <37248488+jonyworker@users.noreply.github.com>
2024-11-08 11:55:45 +0800

    Updates

---
+++ assets/ChangeLog-Cjtxlyfi.js
@@ -0,0 +1,23 @@
+import{af as n,ag as a,ah as s}from"./index-CfKJr9bI.js";import{u as o}from"./index-NfEsEzki.js";import"./iframe-DA8wbGxa.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-DrFu-skq.js";const i=`## V1.0.0 (2024-11-08)\r
+\r
+✨ New Feature\r
+\r
+---\r
+\r
+🆙update\r
+\r
+- 同步學長版 Storybook 目錄\r
+- 資料來源接口皆改名 datasource\r
+- 合併 RadioItem & Radio\r
+- 合併 CheckboxItem & Checkbox\r
+- 不同步部分標注星號待討論\r
+\r
+---\r
+\r
+🪲 Bug Fix\r
+`;function e(r){const t={h1:"h1",...o(),...r.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(a,{title:"Changelog"}),`
+`,n.jsx(t.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(s,{children:i})]})}function g(r={}){const{wrapper:t}={...o(),...r.components};return t?n.jsx(t,{...r,children:n.jsx(e,{...r})}):e(r)}export{g as default};
---
+++ assets/Menu.stories---m_Vpqz.js
@@ -0,0 +1,72 @@
+import{_ as u}from"./Menu-BgQBln_C.js";import"./vue.esm-bundler-wiEVvyCh.js";import"./Icon-HcFZ1N8v.js";import"./iframe-DA8wbGxa.js";import"../sb-preview/runtime.js";const p={title:"Design System/Menu",component:u,tags:["autodocs"],argTypes:{datasource:{description:"Menu 對應的資料陣列",control:{type:"object"}},isExpanded:{description:"Menu 展開收合",control:{type:"boolean"}}},parameters:{docs:{title:"Menu",description:{component:"Menu 組件的呈現及說明。"}}}},e={name:"預設項目",args:{datasource:[{icon:"home",label:"首頁",path:"/"},{icon:"users",label:"用戶管理",path:"/users",children:[{label:"用戶列表",path:"/users/list"},{label:"用戶設置",path:"/users/settings"}]},{icon:"file",label:"文件管理",path:"/files",children:[{label:"所有文件",path:"/files/all"},{label:"已分享",path:"/files/shared"}]},{icon:"mail",label:"訊息中心",path:"/messages"},{icon:"setting",label:"系統設置",path:"/settings"}],isExpanded:!0},render:t=>({components:{Menu:u},setup(){return{args:t}},template:`
+            <div :style="args.isExpanded ? 'width:auto' : 'width:47px'">
+                <Menu
+                    :datasource="args.datasource"
+                    :isExpanded="args.isExpanded"
+                ></Menu>
+            </div>
+        `}),parameters:{controls:{include:["datasource","isExpanded"]}}};var n,a,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  name: "預設項目",
+  args: {
+    datasource: [{
+      icon: "home",
+      label: "首頁",
+      path: "/"
+    }, {
+      icon: "users",
+      label: "用戶管理",
+      path: "/users",
+      children: [{
+        label: "用戶列表",
+        path: "/users/list"
+      }, {
+        label: "用戶設置",
+        path: "/users/settings"
+      }]
+    }, {
+      icon: "file",
+      label: "文件管理",
+      path: "/files",
+      children: [{
+        label: "所有文件",
+        path: "/files/all"
+      }, {
+        label: "已分享",
+        path: "/files/shared"
+      }]
+    }, {
+      icon: "mail",
+      label: "訊息中心",
+      path: "/messages"
+    }, {
+      icon: "setting",
+      label: "系統設置",
+      path: "/settings"
+    }],
+    isExpanded: true
+  },
+  render: args => ({
+    components: {
+      Menu
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+            <div :style="args.isExpanded ? 'width:auto' : 'width:47px'">
+                <Menu
+                    :datasource="args.datasource"
+                    :isExpanded="args.isExpanded"
+                ></Menu>
+            </div>
+        \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      include: ["datasource", "isExpanded"] // 確保包含有效的參數名稱
+    }
+  }
+}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const c=["MenuDefault"];export{e as MenuDefault,c as __namedExportsOrder,p as default};
