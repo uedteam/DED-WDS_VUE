9f9804fa15a69486ffe50d902548c70abe49f2a4
jonyworker <jonywu@gmail.com>
2024-11-16 12:20:54 +0800

    🆙update：Toast - 新增 parameters.docs.source 作為下載程式碼用 - 新增 icon 接口

--- libs/src/ui/element/Toast/Toast.vue
+++ libs/src/ui/element/Toast/Toast.vue
@@ -1,9 +1,9 @@
 <script setup>
-import {ref, onMounted, onUnmounted } from 'vue'
-import Icon from "@/ui/element/Icon/Icon.vue";
+import { ref, onMounted, onUnmounted } from 'vue';
+import Icon from '@/ui/element/Icon/Icon.vue';
 
 // 定義 Emits
-const emit = defineEmits(['close'])
+const emit = defineEmits(['close']);
 
 // 定義 Props
 const props = defineProps({
@@ -13,30 +13,36 @@ const props = defineProps({
 	message: {
 		type: String,
 	},
-	severity:{
+	severity: {
 		type: String,
-		default: 'success',
+		default: "success",
 		validator: (value) =>
-			['success', 'warning', 'error', 'info'].includes(value),
+			["success", "warning", "error", "info"].includes(value),
 	},
 	duration: {
 		type: Number,
 		default: 3000,
 	},
-})
+	icon: {
+		type: String,
+		default: "",
+	},
+});
+
+
 // 控制 關閉
 const handleClose = () => {
 	emit('close');
 };
 
 // 控制 自動關閉
-let timer = ref(null)
+let timer = ref(null);
 const show = () => {
 	if (timer.value) clearTimeout(timer.value);
 	timer.value = setTimeout(() => {
-		handleClose()
+		handleClose();
 	}, props.duration);
-}
+};
 
 // 元件掛載時 setTimeout
 onMounted(() => {
@@ -45,8 +51,8 @@ onMounted(() => {
 
 // 元件卸載時卸載 setTimeout
 onUnmounted(() => {
-	if (timer) clearTimeout(timer)
-})
+	if (timer.value) clearTimeout(timer.value);
+});
 </script>
 
 <template>
@@ -58,20 +64,12 @@ onUnmounted(() => {
 			</button>
 			<!-- toast - 標題及說明文字 -->
 			<p class="message">
-			    <span class="icon-wrapper" :class="`toast-${props.severity}`">
-				    <template v-if="props.severity ==='success'">
-					    <Icon name="check" size="14"></Icon>
-				    </template>
-				    <template v-if="props.severity ==='error'">
-					    <Icon name="close" size="14"></Icon>
-				    </template>
-				    <template v-if="props.severity ==='warning'">
-					    <Icon name="exclamation" size="14"></Icon>
-				    </template>
-				    <template v-if="props.severity ==='info'">
-					    <Icon name="info" size="16"></Icon>
-				    </template>
-			    </span>
+				<span class="icon-wrapper" :class="`toast-${props.severity}`">
+					<Icon
+						:name="props.icon || getDefaultIcon(props.severity)"
+						size="14"
+					></Icon>
+				</span>
 				<span>{{ props.title }}</span>
 			</p>
 			<p class="description">{{ props.message }}</p>
@@ -79,6 +77,23 @@ onUnmounted(() => {
 	</teleport>
 </template>
 
+<script>
+// 返回默認的 icon 名稱
+function getDefaultIcon(severity) {
+	switch (severity) {
+		case 'success':
+			return 'check';
+		case 'error':
+			return 'close';
+		case 'warning':
+			return 'exclamation';
+		case 'info':
+			return 'info';
+		default:
+			return '';
+	}
+}
+</script>
 
 <style scoped>
 
