a069db6765243515d86b7725222a08fae8e8c0dd
jonyworker <jonywu@gmail.com>
2024-11-25 00:48:26 +0800

    🆙update：Dialog - 同步學長ＣＳＳ，還有一些東西待開會確認 (20241122 ok) - 彈出視窗版面照學長的 (20241122 ok) - 彈出視窗參數照學長的 title(必), content(必) confirmText, CancelText, className (20241122 ok) - 安裝Dompurify 確保安全性，在 dialogHeader 的輸入欄位故意添加 攻擊性HTML(經測試可被過濾) (20241122 ok) - 添加參數「className」(20241122 ok)

--- libs/src/ui/element/Dialog/Dialog.vue
+++ libs/src/ui/element/Dialog/Dialog.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Button from '@/ui/element/Button/Button.vue'
 
 // 調用 composable
 import {useDialog} from "@/ui/element/Dialog/useDialog.js";
@@ -6,32 +7,65 @@ const dialog = useDialog();
 
 // 定義 Props
 const props = defineProps({
-
+	title: {
+		type: String,
+		required: true,
+		default: "",
+	},
+	content: {
+		type: String,
+		required: true,
+		default: "",
+	},
+	confirmText: {
+		type: String,
+		default: "確認",
+	},
+	cancelText: {
+		type: String,
+		default: "取消",
+	},
+	className: {
+		type: String,
+		default: "",
+	},
 });
+
 
 </script>
 
 <template>
 	<Teleport to="#dialog">
 		<transition name="dialog">
-			<div class="dialog-mask" v-if="dialog.showDialogStatus.value" @click="dialog.closeDialog()">
-				<div class="dialog-wrapper">
-					<div class="dialog-container">
+			<div class="ded-dialog-overlay" v-if="dialog.showDialogStatus.value" @click="dialog.closeDialog()">
 
-						<div class="dialog-header">
-							<slot name="dialogHeader"></slot>
-						</div>
+				<div :class="{'ded-dialog-content': true, [props.className]: !!props.className}" >
 
-						<div class="dialog-body">
-							<slot name="dialogBody"></slot>
-						</div>
+					<div class="ded-dialog-header">
+						<slot name="dialogHeader">
+							<h2 class="ded-dialog-title">{{ props.title }}</h2>
+						</slot>
+					</div>
 
-						<div class="dialog-footer">
-							<slot name="dialogFooter"></slot>
-						</div>
+					<div class="ded-dialog-body">
+						<slot name="dialogBody">
+							<p>{{ props.content }}</p>
+						</slot>
+					</div>
+
+					<div class="ded-dialog-footer">
+						<slot name="dialogFooter">
+							<Button variant="contained" themeColor="primary" size="medium" className="ded-cancel-btn">
+								{{ props.cancelText }}
+							</Button>
+							<Button variant="contained" themeColor="error" size="medium">
+								{{ props.confirmText }}
+							</Button>
+						</slot>
 					</div>
 				</div>
 			</div>
+
 		</transition>
 	</Teleport>
 </template>
