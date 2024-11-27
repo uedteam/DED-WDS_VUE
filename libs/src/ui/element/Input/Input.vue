9d750456f1aac9d12fc602c5982389296eae8348
jonyworker <jonywu@gmail.com>
2024-11-28 00:06:12 +0800

    🆙update：Input - 圖示參考 kevin 結構多包一層 div並給予對應 class name (20241126 done)

--- libs/src/ui/element/Input/Input.vue
+++ libs/src/ui/element/Input/Input.vue
@@ -90,7 +90,9 @@ const togglePasswordVisibility = () => {
 		props.isDisabled }, ]">
 			<!-- 輸入框 prefix icon -->
 			<template v-if="prefix">
-				<Icon :class="`ded-icon-${size}`" :name="props.prefix" ></Icon>
+				<div :class="`ded-icon-${props.size}`">
+					<Icon :name="props.prefix" ></Icon>
+				</div>
 			</template>
 
 			<!-- 輸入框 -->
@@ -100,22 +102,19 @@ const togglePasswordVisibility = () => {
 				:placeholder="props.placeholder"
 				v-model="modelValue">
 
-			<!-- 輸入框 suffix icon -->
-<!--			<template v-if="suffix">-->
-<!--				<Icon :class="`icon-${size}`" :name="props.suffix" ></Icon>-->
-<!--			</template>-->
-
 			<!-- Suffix Icons -->
 			<template v-if=" modelValue || props.type === 'password'">
 				<!-- input type 等於 text -->
-				<button v-if="modelValue && props.type === 'text'" class="ded-clear-button" @click="clearInput">
-					<Icon name="close" :class="`ded-icon-${props.size}`" />
-				</button>
+				<div v-if="modelValue && props.type === 'text'" :class="`ded-icon-${props.size}`"
+				     @click="clearInput">
+					<Icon name="close" />
+				</div>
 
 				<!-- input type 等於 password -->
-				<button v-if="props.type === 'password'" class="ded-clear-button" @click="togglePasswordVisibility">
-					<Icon :name="showPassword ? 'visibility' : 'visibility_off'" :class="`ded-icon-${props.size}`" />
-				</button>
+				<div v-if="props.type === 'password'" :class="`ded-icon-${props.size}`"
+				     @click="togglePasswordVisibility" >
+					<Icon :name="showPassword ? 'visibility' : 'visibility_off'" />
+				</div>
 			</template>
 		</div>
 
