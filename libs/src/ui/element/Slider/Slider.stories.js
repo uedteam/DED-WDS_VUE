2545fc4dbe1acef17bf7c3224327e186c9ecef97
jonyworker <jonywu@gmail.com>
2024-11-28 23:24:04 +0800

    🆙update：Slider - Design System 改 Component - 刪除不必要註解

--- libs/src/ui/element/Slider/Slider.stories.js
+++ libs/src/ui/element/Slider/Slider.stories.js
@@ -1,9 +1,8 @@
 import Slider from "./Slider.vue";
 import {ref} from "vue";
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
-	title: "Design System/Slider",
+	title: "Component/Slider",
 	component: Slider,
 	tags: ["autodocs"],
 	argTypes: {
@@ -322,117 +321,3 @@ export const SliderColors = {
 		}
 	},
 };
-
-
-
-//--- JONY VERSION START ---//
-
-// // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-// export default {
-// 	title: "Design System/Slider",
-// 	component: Slider,
-// 	tags: ["autodocs"],
-// 	argTypes: {
-// 		themeColor: {
-// 			description: '主題顏色',
-// 			control: { type: 'select' },
-// 			options: [
-// 				'primary',
-// 				'secondary',
-// 				'tertiary',
-// 				'success',
-// 				'warning',
-// 				'error',
-// 				'info',
-// 			],
-// 		},
-// 		min: {
-// 			description: '最小值',
-// 			control: { type: 'number' },
-// 		},
-// 		max: {
-// 			description: '最大值',
-// 			control: { type: 'number' },
-// 		},
-// 		step: {
-// 			description: '步進值',
-// 			control: { type: 'number' },
-// 		},
-// 		initValue: {
-// 			description: '初始值',
-// 			control: { type: 'number' },
-// 		},
-// 		label: {
-// 			description: '單位',
-// 			control: { type: 'text' },
-// 		},
-// 		isDisabled: {
-// 			description: '是否禁用',
-// 			control: { type: 'boolean' },
-// 		},
-// 		className: {
-// 			description: '客製化樣式',
-// 			control: { type: 'text' },
-// 		},
-//
-// 	},
-// 	parameters: {
-// 		// 自動文件
-// 		docs: {
-// 			title: "Slider",
-// 			description: {
-// 				component: "Slider組件的呈現及說明。",
-// 			},
-// 		},
-// 	},
-//
-// 	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-// 	// args: { onClick: fn() },
-// };
-//
-// //==== Slider 基礎樣式 ====//
-// export const SliderDefault = {
-// 	name: "Slider 基礎樣式",
-// 	args: {
-// 		themeColor: "primary",
-// 		min: 0,
-// 		max: 100,
-// 		step: 1,
-// 		initValue: "50",
-// 		label: "℃",
-// 		isDisabled: false
-// 	},
-// 	render: (args) => ({
-// 		components: { Slider },
-// 		setup() {
-// 			const sliderValue = ref(20);
-// 			return {
-// 				args,
-// 				sliderValue
-// 			};
-// 		},
-// 		template: `
-// 			<slider
-// 				:themeColor="args.themeColor"
-// 				:min="args.min"
-// 				:max="args.max"
-// 				:step="args.step"
-// 				:initValue="args.initValue"
-// 				:label="args.label"
-// 				:isDisabled="args.isDisabled"
-// 				v-model="sliderValue"
-// 			></slider>
-//
-// 			ModelValue: {{sliderValue}}
-//         `,
-// 	}),
-// 	// 控制 controls 中能控制的參數
-// 	parameters: {
-// 		controls: {
-// 			// include: ['themeColor', 'label', 'value', 'name' ],
-// 			// exclude:['range']
-// 		},
-// 	},
-// };
-
-//--- JONY VERSION END ---//
