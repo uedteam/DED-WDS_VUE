aef2823feeb4dbc9716c753a802166821259e601
jonyworker <jonywu@gmail.com>
2024-12-18 00:49:22 +0800

    🆙update：StatusIndicator - 新增 StatusIndicator 元件。 - 自網頁複製 CSS (待學長版更後更新)

---
+++ libs/src/ui/element/Status-Indicator/StatusIndicator.vue
@@ -0,0 +1,68 @@
+<script setup>
+import Icon from '@/ui/element/Icon/Icon.vue';
+
+const props = defineProps({
+	themeColor: {
+		type: String,
+		default: "primary",
+		validator: (value) =>
+			[
+				"primary",
+				"secondary",
+				"neutral",
+				"info",
+				"success",
+				"warning",
+				"error",
+			].includes(value),
+	},
+	variant: {
+		type: String,
+		default: "text",
+		validator: (value) => ["text", "filled"].includes(value),
+	},
+	prefix: {
+		type: String,
+		required: true,
+		default: "InfoCircleIcon",
+	},
+	size: {
+		type: String,
+		validator: (value) => ["small", "medium", "large"].includes(value),
+	},
+	className: {
+		type: String,
+		default: "",
+	},
+});
+
+</script>
+
+<template>
+	<div
+		class="status-indicator"
+		:class="{
+			[`status-indicator-${props.variant}-${props.themeColor}`]: props.variant && props.themeColor,
+			[props.className]: !!props.className
+		}"
+		role="status">
+		<template v-if="props.variant === 'text'">
+			<div style="line-height: 1;">
+				<Icon :name="props.prefix" size="20"></Icon>
+			</div>
+		</template>
+		<template v-else>
+			<span class="status-indicator-dot" :class="`status-indicator-${props.themeColor}`"></span>
+		</template>
+
+		<div class="status-indicator-text" :class="`status-indicator-text-${props.variant}`">
+			<slot></slot>
+		</div>
+	</div>
+
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
