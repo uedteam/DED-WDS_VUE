214d70e97e69a2b5a6a13321fd818d48b9d51180
jonyworker <jonywu@gmail.com>
2024-12-22 15:23:41 +0800

    🆙update：Toast - 修改 Vue - 修改 storybook

--- libs/src/ui/element/Toast/Toast.stories.js
+++ libs/src/ui/element/Toast/Toast.stories.js
@@ -1,7 +1,10 @@
 import { useToast } from '@/ui/element/Toast/useToast.js';
+import { computed } from 'vue';
 import Toast from "@/ui/element/Toast/Toast.vue";
 import Icon from "@/ui/element/Icon/Icon.vue";
 import Button from "@/ui/element/Button/Button.vue";
+import Title from '@/ui/element/Title/Title.vue';
+import StatusIndicator from '@/ui/element/Status-Indicator/StatusIndicator.vue';
 
 export default {
 	components: {Icon, Toast},
@@ -13,6 +16,9 @@ export default {
 			description: "主題顏色",
 			control: { type: "select" },
 			options: [
+				"primary",
+				"secondary",
+				"neutral",
 				"info",
 				"success",
 				"warning",
@@ -25,39 +31,44 @@ export default {
 			}
 		},
 		title: {
-			description: "標題",
+			description: "內容",
 			control: { type: "text" },
 		},
-		message: {
+		content: {
 			description: "說明文字",
 			control: { type: "text" },
 		},
-
-		duration: {
-			description: "顯示的持續時間（毫秒）",
-			control: { type: "range",
-						min: 500,
-						max: 10000,
-						step: 500  },
-		},
-		icon: {
-			description: "icon",
+		prefix: {
+			description: "前綴",
 			control: {
 				type: "select",
 				labels: {
-					"": "None",
-					home: "home",
-					folder: "folder",
-					academy: "academy",
-					"arrow-forward": "arrow-forward",
-					"finger-print": "finger-print",
+					"": "none",
+					InfoCircleIcon: "InfoCircleIcon",
+					SuccessCircleIcon: "SuccessCircleIcon",
+					WarningCircleIcon: "WarningCircleIcon",
+					ErrorCircleIcon: "ErrorCircleIcon",
 				}
 			},
-			options: [ "", "home", "folder", "academy", "arrow-forward", "finger-print" ],
+			options: [ '', 'InfoCircleIcon', 'SuccessCircleIcon', 'WarningCircleIcon', 'ErrorCircleIcon' ],
+		},
+		duration: {
+			description: "持續時間",
+			control: { type: "range",
+						min: 500,
+						max: 5000,
+						step: 100  },
 		},
 		className: {
 			description: "客製化樣式",
 			control: { type: "text" },
+		},
+		close: {
+			description: '關閉事件',
+			table: {
+				category: 'Events',
+				type: { summary: 'emits' },
+			},
 		},
 	},
 	parameters: {
@@ -72,14 +83,315 @@ export default {
 };
 
 //==== 預設項目 ====//
-export const ToastDefaultNew = {
+export const ToastDefault = {
 	name: "預設項目",
 	args: {
 		themeColor: "success",
-		title: "Toast Title",
-		message: "Hello, world! This is a toast message.",
+		title: "Title",
+		content: "Content",
+		prefix:'SuccessCircleIcon',
 		duration: 5000,
-		icon: "",
+		className: "",
+	},
+	render: (args) => ({
+		components: { Toast, Button, Title, StatusIndicator, Icon },
+		setup() {
+			const onClose = () => {
+				window.alert('Close');
+			};
+			const toastBorderClass = computed(() => {
+				const color = args.themeColor;
+				return `ded-toast-border-${color}`;
+			});
+			return {
+				args,
+				onClose,
+				toastBorderClass
+			};
+		},
+		template: `
+			<div class="ded-toast" :class="toastBorderClass">
+				<!-- toast - 關閉按鈕 -->
+				<Button class="ded-close-button" variant="text" themeColor="neutral" @click="onClose">
+					<Icon name="close" size="20"></Icon>
+				</Button>
+				<!-- toast - 標題及說明文字 -->
+				<div class="ded-message">
+					<StatusIndicator
+						:themeColor="args.themeColor"
+						variant="text"
+						:prefix="args.prefix"
+					>
+						<Title>{{ args.title }}</Title>
+					</StatusIndicator>
+				</div>
+				<p class="ded-description">{{ args.content }}</p>
+			</div>
+        `,
+	}),
+	parameters: {
+		controls: {
+			// include: ['themeColor', 'label', 'value', 'name' ],
+		},
+		docs: {
+			source: {
+				transform: (src, storyContext) => {
+					const { args } = storyContext;
+					return [
+						'<template>',
+						`  <Toast`,
+						`    v-for="toast in toasts"`,
+						`    :key="toast.id"`,
+						`    :themeColor="toast.themeColor"`,
+						`    :title="${args.title}"`,
+						`    :content="${args.content}"`,
+						`    :prefix="${args.prefix}"`,
+						`    :duration="${args.duration}"`,
+						`    :className="${args.className}"`,
+						`    @close="remove(toast.id)"`,
+						`  ></Toast>`,
+						`  <Button`,
+						`    themeColor="primary"`,
+						`    variant="filled"`,
+						`    radius="md"`,
+						`    @click="showToast"`,
+						`  >`,
+						`    Toast Trigger`,
+						`  </Button>`,
+						'</template>',
+						'',
+						'<script setup>',
+						'  import { useToast } from "@/path/to/useToast";',
+						'',
+						'  const { add, toasts, remove } = useToast();',
+						'',
+						'  const showToast = () => {',
+						'    add({',
+						`      themeColor: "${args.themeColor}",`,
+						`      title: "${args.title}",`,
+						`      content: "${args.content}",`,
+						'    });',
+						'  };',
+						'</script>',
+					].join('\n').trim();
+				}
+			}
+		}
+	},
+};
+
+//==== 通知訊息類型 ====//
+export const ToastTotal = {
+	name: "預設項目",
+	args: {
+		title: "Title",
+		content: "Content",
+		duration: 5000,
+		className: "",
+	},
+	render: (args) => ({
+		components: { Toast, Button, Title, StatusIndicator, Icon },
+		setup() {
+			const onClose = () => {
+				window.alert('Close');
+			};
+			return {
+				args,
+				onClose,
+			};
+		},
+		template: `
+			<div class="ded-toast ded-toast-border-success">
+				<!-- toast - 關閉按鈕 -->
+				<Button class="ded-close-button" variant="text" themeColor="neutral" @click="onClose">
+					<Icon name="close" size="20"></Icon>
+				</Button>
+				<!-- toast - 標題及說明文字 -->
+				<div class="ded-message">
+					<StatusIndicator
+						themeColor="success"
+						variant="text"
+						prefix="SuccessCircleIcon"
+					>
+						<Title>{{ args.title }}</Title>
+					</StatusIndicator>
+				</div>
+				<p class="ded-description">{{ args.content }}</p>
+			</div>
+
+			<div class="ded-toast ded-toast-border-warning">
+				<!-- toast - 關閉按鈕 -->
+				<Button class="ded-close-button" variant="text" themeColor="neutral" @click="onClose">
+					<Icon name="close" size="20"></Icon>
+				</Button>
+				<!-- toast - 標題及說明文字 -->
+				<div class="ded-message">
+					<StatusIndicator
+						themeColor="warning"
+						variant="text"
+						prefix="WarningCircleIcon"
+					>
+						<Title>{{ args.title }}</Title>
+					</StatusIndicator>
+				</div>
+				<p class="ded-description">{{ args.content }}</p>
+			</div>
+
+			<div class="ded-toast ded-toast-border-error">
+				<!-- toast - 關閉按鈕 -->
+				<Button class="ded-close-button" variant="text" themeColor="neutral" @click="onClose">
+					<Icon name="close" size="20"></Icon>
+				</Button>
+				<!-- toast - 標題及說明文字 -->
+				<div class="ded-message">
+					<StatusIndicator
+						themeColor="error"
+						variant="text"
+						prefix="ErrorCircleIcon"
+					>
+						<Title>{{ args.title }}</Title>
+					</StatusIndicator>
+				</div>
+				<p class="ded-description">{{ args.content }}</p>
+			</div>
+
+			<div class="ded-toast ded-toast-border-info">
+				<!-- toast - 關閉按鈕 -->
+				<Button class="ded-close-button" variant="text" themeColor="neutral" @click="onClose">
+					<Icon name="close" size="20"></Icon>
+				</Button>
+				<!-- toast - 標題及說明文字 -->
+				<div class="ded-message">
+					<StatusIndicator
+						themeColor="info"
+						variant="text"
+						prefix="InfoCircleIcon"
+					>
+						<Title>{{ args.title }}</Title>
+					</StatusIndicator>
+				</div>
+				<p class="ded-description">{{ args.content }}</p>
+			</div>
+        `,
+	}),
+	parameters: {
+		controls: {
+			// include: ['themeColor', 'label', 'value', 'name' ],
+		},
+		docs: {
+			source: {
+				transform: (src, storyContext) => {
+					const { args } = storyContext;
+					return [
+						`<Toast`,
+						`    v-for="toast in toasts"`,
+						`    :key="toast.id"`,
+						`    themeColor="success"`,
+						`    :title="${args.title}"`,
+						`    :content="${args.content}"`,
+						`    prefix="SuccessCircleIcon"`,
+						`    :duration="${args.duration}"`,
+						`    className="${args.className}"`,
+						`    @close="remove(toast.id)"`,
+						`></Toast>`,
+						`<Button`,
+						`    themeColor="primary"`,
+						`    variant="filled"`,
+						`    radius="4px"`,
+						`    @click="showToast"`,
+						`>`,
+						`    Toast Trigger`,
+						`</Button>`,
+
+						`<Toast`,
+						`    v-for="toast in toasts"`,
+						`    :key="toast.id"`,
+						`    themeColor="warning"`,
+						`    :title="${args.title}"`,
+						`    :content="${args.content}"`,
+						`    prefix="WarningCircleIcon"`,
+						`    :duration="${args.duration}"`,
+						`    className="${args.className}"`,
+						`    @close="remove(toast.id)"`,
+						`></Toast>`,
+						`<Button`,
+						`    themeColor="primary"`,
+						`    variant="filled"`,
+						`    radius="4px"`,
+						`    @click="showToast"`,
+						`>`,
+						`    Toast Trigger`,
+						`</Button>`,
+
+						`<Toast`,
+						`    v-for="toast in toasts"`,
+						`    :key="toast.id"`,
+						`    themeColor="error"`,
+						`    :title="${args.title}"`,
+						`    :content="${args.content}"`,
+						`    prefix="ErrorCircleIcon"`,
+						`    :duration="${args.duration}"`,
+						`    className="${args.className}"`,
+						`    @close="remove(toast.id)"`,
+						`></Toast>`,
+						`<Button`,
+						`    themeColor="primary"`,
+						`    variant="filled"`,
+						`    radius="4px"`,
+						`    @click="showToast"`,
+						`>`,
+						`    Toast Trigger`,
+						`</Button>`,
+
+						`<Toast`,
+						`    v-for="toast in toasts"`,
+						`    :key="toast.id"`,
+						`    themeColor="info"`,
+						`    :title="${args.title}"`,
+						`    :content="${args.content}"`,
+						`    prefix="InfoCircleIcon"`,
+						`    :duration="${args.duration}"`,
+						`    className="${args.className}"`,
+						`    @close="remove(toast.id)"`,
+						`></Toast>`,
+						`<Button`,
+						`    themeColor="primary"`,
+						`    variant="filled"`,
+						`    radius="4px"`,
+						`    @click="showToast"`,
+						`>`,
+						`    Toast Trigger`,
+						`</Button>`,
+						'',
+						'<script setup>',
+						'  import { useToast } from "@/path/to/useToast";',
+						'',
+						'  const { add, toasts, remove } = useToast();',
+						'',
+						'  const showToast = () => {',
+						'    add({',
+						`      themeColor: "${args.themeColor}",`,
+						`      title: "${args.title}",`,
+						`      content: "${args.content}",`,
+						'    });',
+						'  };',
+						'</script>',
+					].join('\n').trim();
+				}
+			}
+		}
+	},
+};
+
+//==== 互動模式 ====//
+export const ToastInterAction = {
+	name: "互動模式",
+	args: {
+		themeColor: "success",
+		title: "Title",
+		content: "Content",
+		prefix:'SuccessCircleIcon',
+		duration: 5000,
 		className: "",
 	},
 	render: (args) => ({
@@ -88,9 +400,9 @@ export const ToastDefaultNew = {
 			const { add, toasts, remove } = useToast();
 			const showToast = () => {
 				add({
-					title: args.title,
-					message: args.message,
 					themeColor: args.themeColor,
+					title: args.title,
+					content: args.content,
 				});
 			};
 			return {
@@ -106,13 +418,13 @@ export const ToastDefaultNew = {
 				:key="toast.id"
 				:themeColor="toast.themeColor"
 				:title="toast.title"
-				:message="toast.message"
+				:content="toast.content"
+				:prefix="args.prefix"
 				:duration="args.duration"
-				:icon="args.icon"
 				:className="args.className"
 				@close="remove(toast.id)"
 			></Toast>
-			<Button themeColor="primary" variant="filled" radius="md" @click="showToast">
+			<Button themeColor="primary" variant="filled" radius="4px" @click="showToast">
 				Toast Trigger
 			</Button>
         `,
@@ -127,25 +439,25 @@ export const ToastDefaultNew = {
 					const { args } = storyContext;
 					return [
 						'<template>',
-						'  <Toast',
-						'    v-for="toast in toasts"',
-						'    :key="toast.id"',
-						`    themeColor="${args.themeColor}"`,
-						`    title="${args.title}"`,
-						`    message="${args.message}"`,
+						`  <Toast`,
+						`    v-for="toast in toasts"`,
+						`    :key="toast.id"`,
+						`    :themeColor="toast.themeColor"`,
+						`    :title="toast.title"`,
+						`    :content="toast.content"`,
+						`    :prefix="${args.prefix}"`,
 						`    :duration="${args.duration}"`,
-						`    icon="${args.icon}"`,
-						`    className="${args.className}"`,
-						'    @close="remove(toast.id)"',
-						'  ></Toast>',
-						'  <Button',
-						'    width="fit"',
-						'    themeColor="primary"',
-						'    variant="contained"',
-						'    @click="showToast"',
-						'  >',
-						'    Toast Trigger',
-						'  </Button>',
+						`    :className="${args.className}"`,
+						`    @close="remove(toast.id)"`,
+						`  ></Toast>`,
+						`  <Button`,
+						`    themeColor="primary"`,
+						`    variant="filled"`,
+						`    radius="4px"`,
+						`    @click="showToast"`,
+						`  >`,
+						`    Toast Trigger`,
+						`  </Button>`,
 						'</template>',
 						'',
 						'<script setup>',
@@ -155,9 +467,9 @@ export const ToastDefaultNew = {
 						'',
 						'  const showToast = () => {',
 						'    add({',
-						`      title: "${args.title}",`,
-						`      message: "${args.message}",`,
 						`      themeColor: "${args.themeColor}",`,
+						`      title: "${args.title}",`,
+						`      content: "${args.content}",`,
 						'    });',
 						'  };',
 						'</script>',
--- libs/src/ui/element/Toast/Toast.vue
+++ libs/src/ui/element/Toast/Toast.vue
@@ -1,36 +1,41 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 import Icon from '@/ui/element/Icon/Icon.vue';
+import Button from '@/ui/element/Button/Button.vue';
+import Title from '@/ui/element/Title/Title.vue';
+import StatusIndicator from '@/ui/element/Status-Indicator/StatusIndicator.vue';
 
 // 定義 Emits
 const emit = defineEmits(['close']);
 
 // 定義 Props
 const props = defineProps({
-	themeColor: {
-		type: String,
-		default: "success",
-		validator: (value) =>
-			[
-				"info",
-				"success",
-				"warning",
-				"error",
-			].includes(value),
-	},
+    themeColor: {
+        type: String,
+        validator: (value) =>
+            [
+                "primary",
+                "secondary",
+                "neutral",
+                "info",
+                "success",
+                "warning",
+                "error",
+            ].includes(value),
+    },
 	title: {
 		type: String,
 	},
-	message: {
+	content: {
 		type: String,
 	},
+    prefix: {
+        type: String,
+        default: "",
+    },
 	duration: {
 		type: Number,
 		default: 3000,
-	},
-	icon: {
-		type: String,
-		default: "",
 	},
 	className: {
 		type: String,
@@ -68,41 +73,23 @@ onUnmounted(() => {
 	<teleport to="#toast">
 		<div class="ded-toast" :class="[`ded-toast-border-${props.themeColor}`, props.className]">
 			<!-- toast - 關閉按鈕 -->
-			<button class="ded-close-button" style="cursor: pointer" @click="handleClose">
+			<Button class="ded-close-button" variant="text" themeColor="neutral" @click="handleClose">
 				<Icon name="close" size="20"></Icon>
-			</button>
+			</Button>
 			<!-- toast - 標題及說明文字 -->
-			<p class="ded-message">
-				<span class="ded-icon-wrapper" :class="`ded-toast-${props.themeColor}`">
-					<Icon
-						:name="props.icon || getDefaultIcon(props.themeColor)"
-						size="14"
-					></Icon>
-				</span>
-				<span>{{ props.title }}</span>
-			</p>
-			<p class="ded-description">{{ props.message }}</p>
+			<div class="ded-message">
+                <StatusIndicator
+                    :themeColor="props.themeColor"
+                    variant="text"
+                    :prefix="props.prefix"
+                >
+                    <Title>{{ props.title }}</Title>
+                </StatusIndicator>
+			</div>
+			<p class="ded-description">{{ props.content }}</p>
 		</div>
 	</teleport>
 </template>
-
-<script>
-// 返回默認的 icon 名稱
-function getDefaultIcon(themeColor) {
-	switch (themeColor) {
-		case 'success':
-			return 'check';
-		case 'error':
-			return 'close';
-		case 'warning':
-			return 'exclamation';
-		case 'info':
-			return 'info';
-		default:
-			return '';
-	}
-}
-</script>
 
 <style scoped>
 
