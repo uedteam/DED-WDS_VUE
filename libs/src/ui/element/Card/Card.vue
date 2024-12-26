fa380410ce6263d7e856878f5784e474701501ae
jonyworker <jonywu@gmail.com>
2024-12-27 01:07:51 +0800

    🆙update：Card - 修改 Vue (學 #74) - 修改 Storybook (學 #74)

---
+++ libs/src/ui/element/Card/Card.vue
@@ -0,0 +1,41 @@
+<script setup>
+// 定義 Props
+const props = defineProps({
+	hasHeaderDivider: {
+		type: Boolean,
+		default: false
+	},
+	className: {
+		type: String,
+		default: "",
+	},
+})
+</script>
+
+<template>
+	<div :class="{
+		'ded-card':true,
+		'ded-card-container': true,
+		 [props.className]: !!props.className}
+	">
+		<div :class="{'ded-card-header': true, 'ded-card-header-divider': props.hasHeaderDivider }">
+			<slot name="cardHeader">
+			</slot>
+		</div>
+
+		<div class="ded-card-body">
+			<slot>
+			</slot>
+		</div>
+
+		<div class="ded-card-footer">
+			<slot name="cardFooter">
+			</slot>
+		</div>
+
+	</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
