0b18b265d4c6434b1634c93417a99e4fb7fca1f4
jony wu <37248488+jonyworker@users.noreply.github.com>
2025-01-19 21:31:49 +0800

    🆙update：SideNav - 重構 Vue (學 #75)   1. 還原themeColor - 修改 Storybook (學 #75)   1. 重構屬性   - 修改 SCSS (學 #75)     1. ded-side-nav-mobile padding

--- libs/src/ui/element/SideNav/SideNav.stories.js
+++ libs/src/ui/element/SideNav/SideNav.stories.js
@@ -1,98 +1,180 @@
-import SideNav from "./SideNav.vue";
+import SideNav from './SideNav.vue';
+
+const dataSource = [
+  {
+    title: 'Home',
+    prefix: 'SvgHome',
+    path: '/Home',
+  },
+  {
+    title: 'User',
+    prefix: 'SvgUser',
+    path: '/users',
+    children: [
+      {
+        title: 'Profile',
+        path: '/user/profile',
+      },
+      {
+        title: 'Account',
+        path: '/user/account',
+      },
+    ],
+  },
+  {
+    title: 'Chart',
+    prefix: 'SvgBarChart',
+    path: '/chart',
+    children: [
+      {
+        title: 'Profile',
+        path: '/chart/profile',
+      },
+      {
+        title: 'Account',
+        path: '/chart/account',
+      },
+      {
+        title: 'Account',
+        path: '/chart/account',
+      },
+      {
+        title: 'Account',
+        path: '/chart/account',
+      },
+    ],
+  },
+  {
+    title: 'Database',
+    prefix: 'SvgDatabase',
+    path: '/database',
+  },
+  {
+    title: 'Favorite',
+    prefix: 'SvgFavorite',
+    path: '/favorite',
+  },
+  {
+    title: 'Calendar',
+    prefix: 'SvgCalendar',
+    path: '/calendar',
+  },
+  {
+    title: 'Notification',
+    prefix: 'SvgNotification',
+    path: '/notification',
+  },
+  {
+    title: 'Language',
+    prefix: 'SvgLanguage',
+    path: '/language',
+  },
+];
 
 export default {
-	title: "Component/SideNav",
-	component: SideNav,
-	tags: ["autodocs"],
-	argTypes: {
-		navItems: {
-			description: "SideNav 對應的資料陣列",
-			control: { type: "object" },
-		},
-		themeColor: {
-			description: '主題顏色',
-			control: { type: 'select' },
-			options: ['None', 'blue', 'yellow', 'grape', 'black'],
-			// mapping: {
-			// 	'None': null,
-			// },
-		},
-	},
-	parameters: {
-		// 自動文件
-		docs: {
-			title: "SideNav",
-			description: {
-				component: "SideNav 組件的呈現及說明。",
-			},
-		},
-	},
+  title: 'Component/SideNav',
+  component: SideNav,
+  tags: ['autodocs'],
+  argTypes: {
+    themeColor: {
+      description: '背景顏色',
+      control: { type: 'select' },
+      options: ['blue', 'yellow', 'grape', 'black'],
+      table: {
+        type: {
+          summary: 'blue | yellow | grape | black',
+        },
+      },
+    },
+    logoSrc: {
+      description: 'Logo 圖片',
+      control: { type: 'text' },
+    },
+    logo: {
+      description: 'Logo 圖示',
+      control: { type: 'text' },
+    },
+    logoLink: {
+      description: 'Logo 連結',
+      control: { type: 'text' },
+    },
+    hasRWD: {
+      description: '是否有側邊導覽',
+      control: { type: 'boolean' },
+    },
+    hasLogo: {
+      description: '是否有 Logo',
+      control: { type: 'boolean' },
+    },
+    hasSearch: {
+      description: '是否有搜尋欄',
+      control: { type: 'boolean' },
+    },
+    dataSource: {
+      description: '資料來源',
+      control: { type: 'object' },
+      table: {
+        type: {
+          summary:
+            '{ title: string; prefix: string; path: string; children?: [ title: string; prefix: string; path:string;] }[]',
+        },
+      },
+    },
+    className: {
+      description: '客製化樣式',
+      control: { type: 'text' },
+    },
+  },
+  parameters: {
+    // 自動文件
+    docs: {
+      title: 'SideNav',
+      description: {
+        component: '側邊導覽組件的呈現及說明。',
+      },
+    },
+  },
 };
 
 //==== 預設項目 ====//
 export const SideNavDefault = {
-	name: "預設項目",
-	args: {
-		navItems: [
-			{
-				icon: "home",
-				label: "首頁",
-				path: "/",
-			},
-			{
-				icon: "users",
-				label: "用戶管理",
-				path: "/users",
-				children: [
-					{ label: "用戶列表 ", path: "/users/list" },
-					{ label: "用戶設置", path: "/users/settings" },
-				],
-			},
-			{
-				icon: "file",
-				label: "文件管理",
-				path: "/files",
-				children: [
-					{ label: "所有文件", path: "/files/all" },
-					{ label: "已分享", path: "/files/shared" },
-				],
-			},
-			{
-				icon: "mail",
-				label: "訊息中心",
-				path: "/messages",
-			},
-			{
-				icon: "setting",
-				label: "系統設置",
-				path: "/settings",
-			},
-		],
-		themeColor: "blue"
-	},
-	render: (args) => ({
-		components: { SideNav },
-		setup() {
-			return {
-				args,
-			};
-		},
-		template: `
-			<div style="height: 480px; overflow: hidden">
-				<SideNav
-					:navItems="args.navItems"
-					:themeColor="args.themeColor"
-				></SideNav>
-			</div>
+  name: '預設項目',
+  args: {
+    themeColor: 'blue',
+    logoSrc: 'https://storage.googleapis.com/ded-wds-bucket/AUO_LOGO.svg',
+    logo: 'SvgAuo',
+    logoLink: 'https://www.auo.com',
+    hasRWD: false,
+    hasLogo: true,
+    hasSearch: true,
+    dataSource: dataSource,
+    className: '',
+  },
+  render: (args) => ({
+    components: { SideNav },
+    setup() {
+      return {
+        args,
+      };
+    },
+    template: `
+			<SideNav
+				:themeColor="args.themeColor"
+				:logoSrc="args.logoSrc"
+				:logo="args.logo"
+				:logoLink="args.logoLink"
+				:hasRWD="args.hasRWD"
+				:hasLogo="args.hasLogo"
+				:hasSearch="args.hasSearch"
+				:dataSource="args.dataSource"
+				:className="args.className"
+			></SideNav>
         `,
-	}),
-	// 控制 controls 中能控制的參數
-	parameters: {
-		controls: {
-			// include: ['themeColor', 'label', 'value', 'name' ],
-		},
-	},
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      // include: ['themeColor', 'label', 'value', 'name' ],
+    },
+  },
 };
-
-
-
