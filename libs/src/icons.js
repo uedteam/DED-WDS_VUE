b97c3d5634a5e54494b3ed73f9c7ac90db9a711a
jony wu <37248488+jonyworker@users.noreply.github.com>
2025-02-25 21:32:13 +0800

    🆙update: Icon - 修改 vue   1. 確保 NPM 打包後仍能正常運作 - 新增 icons.js   1. 確保 NPM 打包後仍能正常運作

---
+++ libs/src/icons.js
@@ -0,0 +1,9 @@
+// src/icons.js
+const icons = import.meta.glob('./assets/icon/*.svg', { eager: true });
+
+export default Object.fromEntries(
+    Object.entries(icons).map(([key, value]) => {
+        const name = key.match(/\/([^/]+)\.svg$/)[1]; // 取得 SVG 檔案名稱
+        return [name, value.default];
+    })
+);
