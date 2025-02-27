4a82a00d1ca0c325df4660102a710006807062f0
Kevin <kk2229.yang@gmail.com>
2025-02-27 12:32:01 +0800

    Updates

---
+++ assets/Breadcrumb.stories-DFFE_aNS.js
@@ -0,0 +1,66 @@
+import{o as a,c as o,t as C,n as E,r as b,w as P,g as v,q,A as j,a as i,F as S,j as y,k as _,x as z,e as A,T as F}from"./vue.esm-bundler-Suj5BAdg.js";const I=["href"],D={__name:"BreadcrumbItem",props:{label:{type:String},href:{type:String},isCurrentPage:{type:Boolean}},setup(n){const r=n;return(s,u)=>(a(),o("a",{class:E(r.isCurrentPage?"ded-breadcrumb-item-label":"ded-breadcrumb-item-link"),href:r.href},C(r.label),11,I))}};D.__docgenInfo={exportName:"default",displayName:"BreadcrumbItem",description:"",tags:{},props:[{name:"label",type:{name:"string"}},{name:"href",type:{name:"string"}},{name:"isCurrentPage",type:{name:"boolean"}}],sourceFiles:["/Users/auouser/Desktop/Work_Space/DED-WDS_VUE/libs/src/ui/element/Breadcrumb/BreadcrumbItem.vue"]};const U={class:"ded-breadcrumb"},V={key:0,class:"ded-rest"},O={class:"ded-rest-dropdown-menu"},W=["href"],f={__name:"Breadcrumb",props:{dataSource:{type:Array,required:!0},className:{type:String,default:""}},setup(n){const r=n,s=b([...r.dataSource]);P(()=>r.dataSource,e=>{s.value=[...e]});const u=v(()=>{const e=s.value;return e.length>3?[e[0],{label:"...",href:"#"},...e.slice(-2)]:e}),L=v(()=>{const e=s.value;return e.length>3?e.slice(1,-2):[]}),c=b(!1),d=b({top:0,left:0}),$=e=>{if(c.value=!c.value,c.value){const t=e.target.getBoundingClientRect();d.value={top:t.bottom+window.scrollY+8,left:t.left+window.scrollX},document.addEventListener("click",m)}else document.removeEventListener("click",m)},m=e=>{const t=document.querySelector(".ded-rest"),l=document.querySelector(".ded-rest-label");t&&l&&!t.contains(e.target)&&!l.contains(e.target)&&(c.value=!1,document.removeEventListener("click",m))},g=()=>{if(c.value){const e=document.querySelector(".ded-rest-label");if(e){const t=e.getBoundingClientRect();d.value={top:t.bottom+window.scrollY+8,left:t.left+window.scrollX}}}};return q(()=>{window.addEventListener("resize",g)}),j(()=>{window.removeEventListener("resize",g),document.removeEventListener("click",m)}),(e,t)=>(a(),o("nav",{class:E({"ded-breadcrumb-container":!0,[r.className]:!!r.className})},[i("ol",U,[(a(!0),o(S,null,y(u.value,(l,h)=>(a(),o("li",{class:"ded-breadcrumb-item",key:h},[l.label==="..."?(a(),o("div",V,[i("div",{class:"ded-rest-label",onClick:$},"..."),(a(),_(F,{to:"body"},[c.value?(a(),o("div",{key:0,class:"ded-dropdown-menu",style:z({top:`${d.value.top}px`,left:`${d.value.left}px`,position:"absolute"})},[i("ul",O,[(a(!0),o(S,null,y(L.value,(B,x)=>(a(),o("li",{class:"ded-drop-item",key:x},[i("a",{class:"ded-drop-item-link",href:B.href},C(B.label),9,W)]))),128))])],4)):A("",!0)]))])):(a(),_(D,{key:1,label:l.label,href:l.href,isCurrentPage:h===u.value.length-1},null,8,["label","href","isCurrentPage"]))]))),128))])],2))}};f.__docgenInfo={exportName:"default",displayName:"Breadcrumb",description:"",tags:{},props:[{name:"dataSource",type:{name:"array"},required:!0},{name:"className",type:{name:"string"},defaultValue:{func:!1,value:'""'}}],sourceFiles:["/Users/auouser/Desktop/Work_Space/DED-WDS_VUE/libs/src/ui/element/Breadcrumb/Breadcrumb.vue"]};function M(n){return`[
+        ${n.map(r=>`{
+            label: "${r.label}",
+            href: "${r.href}",
+        }`).join(`,
+    `)}
+    ]`}const T={title:"Component/Breadcrumb",component:f,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",control:{type:"object"},table:{type:{summary:"{ label: string; href?: string | undefined; }[]"}}},className:{description:"客製化樣式",control:{type:"text"}}},parameters:{deepControls:{enabled:!0},docs:{title:"Breadcrumb",description:{component:"Breadcrumb 組件的呈現及說明。"}}}},p={name:"預設項目",args:{dataSource:[{label:"Home",href:"/"},{label:"Level1",href:"/first"},{label:"Level2",href:"https://tw.yahoo.com"},{label:"Level3",href:"/first/second/third/n"},{label:"Current"}],className:""},render:n=>({components:{Breadcrumb:f},setup(){return{args:n}},template:`
+            <Breadcrumb
+                :dataSource="args.dataSource"
+                :className="args.className"
+            ></Breadcrumb>
+        `}),parameters:{controls:{include:["dataSource","className"]},docs:{source:{transform:(n,r)=>{const{args:s}=r;return["<script setup>",'import Breadcrumb from "@/ui/element/Breadcrumb/Breadcrumb.vue";',"<\/script>","","<template>","  <Breadcrumb",`    :dataSource='${M(s.dataSource)}'`,`    ${s.className?`className="${s.className}"`:""}`,"  >","  </Breadcrumb>","</template>"].join(`
+`).trim()}}}}};var N,k,w;p.parameters={...p.parameters,docs:{...(N=p.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  name: "預設項目",
+  args: {
+    dataSource: [{
+      "label": "Home",
+      "href": "/"
+    }, {
+      "label": "Level1",
+      "href": "/first"
+    }, {
+      "label": "Level2",
+      "href": "https://tw.yahoo.com"
+    }, {
+      "label": "Level3",
+      "href": "/first/second/third/n"
+    }, {
+      "label": "Current"
+    }],
+    className: ""
+  },
+  render: args => ({
+    components: {
+      Breadcrumb
+    },
+    setup() {
+      return {
+        args
+      };
+    },
+    template: \`
+            <Breadcrumb
+                :dataSource="args.dataSource"
+                :className="args.className"
+            ></Breadcrumb>
+        \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      include: ['dataSource', 'className']
+    },
+    docs: {
+      source: {
+        transform: (src, storyContext) => {
+          const {
+            args
+          } = storyContext;
+          const dataSourceString = formatDataSource(args.dataSource);
+          return [\`<script setup>\`, \`import Breadcrumb from "@/ui/element/Breadcrumb/Breadcrumb.vue";\`, \`<\/script>\`, '', '<template>', '  <Breadcrumb', \`    :dataSource='\${dataSourceString}'\`, \`    \${args.className ? \`className="\${args.className}"\` : ""}\`, '  >', '  </Breadcrumb>', '</template>'].join('\\n').trim();
+        }
+      }
+    }
+  }
+}`,...(w=(k=p.parameters)==null?void 0:k.docs)==null?void 0:w.source}}};const H=["BreadcrumbMany"];export{p as BreadcrumbMany,H as __namedExportsOrder,T as default};
---
+++ assets/ChangeLog-DZmYQSTq.js
@@ -0,0 +1,91 @@
+import{af as n,ag as r,ak as i}from"./index-BjOlPhG-.js";import{u as a}from"./index-DUBRHsCD.js";import"./iframe-DAf9t8bU.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-DrFu-skq.js";const s=`## V1.0.0 (2025-02-24)
+
+# 檢整所有元件 showCode。經測試所有元件皆可拷貝 showCode 於專案內正常使用
+
+# 尚須調整：調整 import 路徑為打包 npm 的路徑
+
+## 🆙 Updates
+
+### **Accordion**
+
+### **Accordion**
+
+### **Avatar**
+
+### **AvatarGroup**
+
+### **Badge**
+
+### **Breadcrumb**
+
+### **Button**
+
+### **Card**
+
+### **CardSimple**
+
+### **Checkbox**
+
+### **DatePicker**
+
+### **Dialog**
+
+### **Divider**
+
+### **Grid**
+
+### **Icon**
+
+### **Image**
+
+### **Input**
+
+### **Layout**
+
+### **List**
+
+### **Menu**
+
+### **Navbar**
+
+### **Pagination**
+
+### **CircleProgress**
+
+### **LineProgress**
+
+### **Radio**
+
+### **Search**
+
+### **Select**
+
+### **SideNav**
+
+### **Slider**
+
+### **SliderControl**
+
+### **StatusIndicator**
+
+### **Stepper**
+
+### **Tabs**
+
+### **Tag**
+
+### **Textarea**
+
+### **Tooltip**
+
+### **Title**
+
+### **Toast**
+
+### **Table**
+`;function t(e){const o={h1:"h1",...a(),...e.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(r,{title:"Introduction/Changelog"}),`
+`,n.jsx(o.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(i,{children:s})]})}function g(e={}){const{wrapper:o}={...a(),...e.components};return o?n.jsx(o,{...e,children:n.jsx(t,{...e})}):t(e)}export{g as default};
