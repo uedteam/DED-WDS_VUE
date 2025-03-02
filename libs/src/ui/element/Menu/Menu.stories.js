2f9bfb64da441fdfdcda1562ca6eac5b5e276cf3
jony wu <37248488+jonyworker@users.noreply.github.com>
2025-03-03 07:17:18 +0800

    🆙update: Menu (Amos簡報#10) - 修改 storybook   1. 同步學長 dataSource 的 order 型別改 number

--- libs/src/ui/element/Menu/Menu.stories.js
+++ libs/src/ui/element/Menu/Menu.stories.js
@@ -30,7 +30,8 @@ export default {
 			control: { type: "object" },
 			table: {
 				type: {
-					summary: "{ label: string; path: string; prefix: string; order: string; isDisabled: boolean; children?: [title:string; path:string; prefix:string; order: string;] }[]"
+					summary: "{ label: string; path: string; prefix: string; order: number; isDisabled: boolean;" +
+						" children?: [title:string; path:string; prefix:string; order: number;] }[]"
 				}
 			}
 		},
@@ -76,24 +77,24 @@ export const MenuDefault = {
 				label: "Dashboard",
 				path: "/",
 				prefix: "SvgHome",
-				order: "1",
+				order: 1,
 				isDisabled: true,
 			},
 			{
 				label: "Profile",
 				path: "/users",
 				prefix: "SvgUser",
-				order: "2",
+				order: 2,
 				children: [
-					{ label: "Contact", path: "/users/Contact", prefix: "SvgMail", order: "1", },
-					{ label: "Password", path: "/users/Password", prefix: "SvgLock", order: "2", },
+					{ label: "Contact", path: "/users/Contact", prefix: "SvgMail", order: 1, },
+					{ label: "Password", path: "/users/Password", prefix: "SvgLock", order: 2, },
 				],
 			},
 			{
 				label: "Setting",
 				path: "/settings",
 				prefix: "SvgSettings",
-				order: "3",
+				order: 3,
 			},
 		],
 		isCollapsed: false,
