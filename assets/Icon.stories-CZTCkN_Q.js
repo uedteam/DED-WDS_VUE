0142c6e4c088b991d2b9891e24d81402c1b96633
Kevin <kk2229.yang@gmail.com>
2025-04-07 11:22:32 +0800

    Updates

---
+++ assets/Icon.stories-CZTCkN_Q.js
@@ -0,0 +1,148 @@
+import{_ as S,a as f,b as I,c as C,d as w,e as y,f as M,g as x,h as k,i as A,j as D,k as L,l as N,m as F,n as $,o as O,p as T,q as B,r as H,s as E,t as j,u as P,v as V,w as U,x as W,y as q,z as G,A as Q,B as J,C as K,D as R,E as X,F as Y,G as Z,H as ss,I as os,J as es,K as ns,L as cs,M as is,N as ts,O as as,P as rs,Q as ls,R as _s,S as vs,T as gs,U as ms,V as bs,W as us,X as ps,Y as ds}from"./SvgWindow-CNB7Yk_m.js";import{c as u,o as p,a as e,v as d}from"./vue.esm-bundler-CC1hEtM-.js";import{_ as a}from"./Icon-DU5IBFbg.js";const zs={xmlns:"http://www.w3.org/2000/svg",fill:"currentColor",viewBox:"0 0 18 18"};function z(s,o){return p(),u("svg",zs,o[0]||(o[0]=[e("mask",{id:"a",width:"18",height:"18",x:"0",y:"0",maskUnits:"userSpaceOnUse",style:{"mask-type":"alpha"}},[e("path",{d:"M0 0h18v18H0z"})],-1),e("g",{mask:"url(#a)"},[e("path",{d:"M9 11.25 5.25 7.519h7.5z"})],-1)]))}const hs={render:z},Ss=Object.freeze(Object.defineProperty({__proto__:null,default:hs,render:z},Symbol.toStringTag,{value:"Module"})),fs={xmlns:"http://www.w3.org/2000/svg",width:"90",height:"30",fill:"#fff",style:{width:"100%",height:"100%"}};function h(s,o){return p(),u("svg",fs,o[0]||(o[0]=[e("mask",{id:"a"},[e("path",{d:"M67.484 23.077c-3.971.003-4.812-4.553-4.812-8.344 0-3.802.84-8.343 4.818-8.346 3.965.003 4.811 4.55 4.811 8.34 0 3.803-.84 8.356-4.817 8.35M67.49.503C61.233.49 55.745 3.34 53.799 9.556l-.307.004-.003-7.985-9.492.006L44 16.194c-.003 3.594-.97 5.239-3.773 5.241-2.956-.003-3.766-1.771-3.766-5.238l.004-14.616h-9.388l-.005 13.61c0 .537.062.983.09 1.483l-.24.07-6.167-15.161-9.403-.003L0 29.5h10.504l5.418-16.987h.125l.022.003.113-.003L21.606 29.5l10.5-.003-1.984-4.877.167-.08c2.172 1.865 5.468 2.61 9.976 2.61 7.856.003 11.886-2.33 12.924-8.403l.267-.003c1.6 7.034 7.4 10.213 14.028 10.21 7.796.006 14.499-4.331 14.508-14.227C81.988 4.82 75.287.5 67.49.5"})],-1),e("path",{d:"M67.484 23.077c-3.971.003-4.812-4.553-4.812-8.344 0-3.802.84-8.343 4.818-8.346 3.965.003 4.811 4.55 4.811 8.34 0 3.803-.84 8.356-4.817 8.35M67.49.503C61.233.49 55.745 3.34 53.799 9.556l-.307.004-.003-7.985-9.492.006L44 16.194c-.003 3.594-.97 5.239-3.773 5.241-2.956-.003-3.766-1.771-3.766-5.238l.004-14.616h-9.388l-.005 13.61c0 .537.062.983.09 1.483l-.24.07-6.167-15.161-9.403-.003L0 29.5h10.504l5.418-16.987h.125l.022.003.113-.003L21.606 29.5l10.5-.003-1.984-4.877.167-.08c2.172 1.865 5.468 2.61 9.976 2.61 7.856.003 11.886-2.33 12.924-8.403l.267-.003c1.6 7.034 7.4 10.213 14.028 10.21 7.796.006 14.499-4.331 14.508-14.227C81.988 4.82 75.287.5 67.49.5"},null,-1),e("path",{d:"m67.484 23.077.001-1h-.002zm.006-16.69v-1h-.001zm-13.691 3.17.01 1 .727-.008.217-.694zm-.307.003h-1v1.01l1.01-.01zm-.003-7.985h1v-1h-1zm-9.492.006v-1h-1v1zM44 16.194h1zm-3.773 5.241-.001 1h.002zm-3.766-5.238h-1zm.004-14.616h1v-1h-1zm-9.388 0v-1h-1v1zm-.005 13.61-1-.001zm.09 1.483.277.961.768-.22-.046-.798zm-.24.07-.927.377.339.832.864-.248zM20.754 1.583l.926-.377-.254-.623h-.672zm-9.403-.003v-1h-.673l-.253.623zM0 29.5l-.926-.377-.56 1.377H0zm10.504 0v1h.73l.222-.696zm5.418-16.987v-1h-.73l-.222.696zm.125 0 .141-.99-.07-.01h-.071zm.022.003-.142.99.085.012.084-.002zm.113-.003.952-.304-.229-.716-.751.02zM21.606 29.5l-.952.304.222.696h.73zm10.5-.003v1h1.485l-.56-1.377zm-1.984-4.877-.43-.902-.85.406.354.872zm.167-.08.65-.758-.494-.425-.588.281zm9.976 2.61v-1zm12.924-8.403-.012-1-.833.01-.14.821zm.267-.003.975-.222-.179-.788-.807.01zm14.028 10.21v-1zm14.508-14.227 1 .001v-.001zm-14.509 7.35c-.787 0-1.368-.22-1.82-.564-.466-.355-.852-.887-1.158-1.586-.623-1.421-.833-3.341-.833-5.193h-2c0 1.938.21 4.192 1 5.995.4.913.97 1.757 1.778 2.374.822.627 1.836.975 3.035.974zm-3.81-7.344c0-1.858.21-3.776.832-5.196.307-.698.693-1.229 1.16-1.584.453-.344 1.036-.565 1.825-.566l-.001-2c-1.199.001-2.213.349-3.035.975-.81.616-1.38 1.459-1.78 2.372-.792 1.802-1.002 4.055-1.002 6zm3.816-7.346c.786 0 1.366.221 1.819.567.466.355.852.886 1.159 1.585.623 1.42.834 3.337.834 5.189h2c0-1.94-.211-4.19-1.003-5.993-.4-.912-.969-1.755-1.777-2.371-.821-.627-1.834-.976-3.03-.977zm3.812 7.34c0 1.859-.21 3.78-.833 5.202q-.46 1.051-1.159 1.585c-.453.344-1.035.564-1.824.563l-.002 2c1.199.002 2.214-.345 3.037-.971.81-.617 1.38-1.46 1.78-2.374.791-1.805 1.001-4.06 1.001-6.004zM67.491-.496c-6.56-.012-12.534 3.005-14.647 9.755l1.909.597c1.779-5.683 6.781-8.363 12.735-8.352zM53.789 8.557l-.306.003.02 2 .307-.004zm.704 1.002-.003-7.984h-2l.003 7.985zM53.488.575 43.996.58l.002 2 9.491-.006zM42.997 1.58 43 16.194h2L44.997 1.58zM43 16.193c-.001 1.757-.249 2.804-.66 3.401-.348.505-.922.84-2.114.841l.002 2c1.61-.001 2.921-.49 3.759-1.706.774-1.124 1.011-2.697 1.013-4.534zm-2.772 4.242c-1.263-.001-1.826-.362-2.156-.867-.4-.613-.611-1.661-.611-3.371h-2c0 1.757.193 3.326.937 4.465.814 1.247 2.134 1.771 3.828 1.773zm-2.767-4.238.004-14.616h-2l-.004 14.615zM36.465.58h-9.388v2h9.388zm-10.388 1-.005 13.609h2l.006-13.609zm-.005 13.61c0 .565.07 1.147.092 1.54l1.997-.114c-.035-.608-.09-.916-.09-1.427zm.814.522-.241.07.553 1.922.241-.07zm.962.654L21.68 1.206l-1.853.754 6.167 15.16zM20.755.583 11.352.58v2l9.402.003zm-10.33.62L-.925 29.123l1.852.754L12.28 1.957zM0 30.5h10.504v-2H0zm11.456-.696 5.42-16.987-1.906-.608-5.42 16.987zm4.466-16.29h.125v-2h-.125zm-.017-.01.022.002.283-1.98-.022-.003zm.191.012.113-.003-.055-2-.113.004zm-.867-.699 5.425 16.987 1.905-.608-5.425-16.987zM21.607 30.5l10.498-.003v-2l-10.499.003zm11.424-1.38-1.982-4.877-1.853.753 1.983 4.878zm-2.477-3.598.166-.08-.863-1.804-.166.08zm-.917-.223c2.453 2.105 6.055 2.85 10.628 2.85v-2c-4.443 0-7.433-.743-9.325-2.367zm10.627 2.85c3.999.002 7.198-.585 9.554-2.078 2.412-1.527 3.801-3.909 4.357-7.156l-1.972-.337c-.483 2.826-1.628 4.647-3.455 5.804-1.881 1.191-4.625 1.77-8.483 1.767zM53.2 19.748l.268-.003-.023-2-.268.003zm-.719-.782c.855 3.759 2.85 6.532 5.544 8.352 2.678 1.809 5.992 2.639 9.46 2.637l-.001-2c-3.162.002-6.062-.756-8.34-2.295-2.262-1.527-3.968-3.86-4.713-7.137zm15.002 10.99c4.073.002 7.954-1.129 10.834-3.649 2.9-2.538 4.67-6.388 4.675-11.578l-2-.002c-.004 4.706-1.59 7.973-3.993 10.075-2.423 2.122-5.791 3.156-9.514 3.153zm15.509-15.228c-.002-5.195-1.77-9.044-4.67-11.581C75.443.628 71.562-.5 67.49-.5v2c3.725 0 7.092 1.032 9.515 3.151 2.4 2.1 3.985 5.365 3.987 10.077z",mask:"url(#a)"},null,-1)]))}const Is={render:h},Cs=Object.freeze(Object.defineProperty({__proto__:null,default:Is,render:h},Symbol.toStringTag,{value:"Module"})),ws=Object.assign({"/libs/src/assets/icons/SvgAccount.svg":ds,"/libs/src/assets/icons/SvgAdd.svg":ps,"/libs/src/assets/icons/SvgAir.svg":us,"/libs/src/assets/icons/SvgArrowDown.svg":bs,"/libs/src/assets/icons/SvgArrowDropDown.svg":Ss,"/libs/src/assets/icons/SvgArrowLeft.svg":ms,"/libs/src/assets/icons/SvgAuo.svg":Cs,"/libs/src/assets/icons/SvgBarChart.svg":gs,"/libs/src/assets/icons/SvgBusy.svg":vs,"/libs/src/assets/icons/SvgCalendar.svg":_s,"/libs/src/assets/icons/SvgCheck.svg":ls,"/libs/src/assets/icons/SvgClock.svg":rs,"/libs/src/assets/icons/SvgClose.svg":as,"/libs/src/assets/icons/SvgCurtain.svg":ts,"/libs/src/assets/icons/SvgDatabase.svg":is,"/libs/src/assets/icons/SvgDisableCircle.svg":cs,"/libs/src/assets/icons/SvgDoor.svg":ns,"/libs/src/assets/icons/SvgErrorCircle.svg":es,"/libs/src/assets/icons/SvgFacebook.svg":os,"/libs/src/assets/icons/SvgFan.svg":ss,"/libs/src/assets/icons/SvgFavorite.svg":Z,"/libs/src/assets/icons/SvgFirstLastPage.svg":Y,"/libs/src/assets/icons/SvgGoogle.svg":X,"/libs/src/assets/icons/SvgHome.svg":R,"/libs/src/assets/icons/SvgHumidity.svg":K,"/libs/src/assets/icons/SvgIdle.svg":J,"/libs/src/assets/icons/SvgInfo.svg":Q,"/libs/src/assets/icons/SvgInfoCircle.svg":G,"/libs/src/assets/icons/SvgLanguage.svg":q,"/libs/src/assets/icons/SvgLight.svg":W,"/libs/src/assets/icons/SvgLine.svg":U,"/libs/src/assets/icons/SvgLocation.svg":V,"/libs/src/assets/icons/SvgLock.svg":P,"/libs/src/assets/icons/SvgLogout.svg":j,"/libs/src/assets/icons/SvgMail.svg":E,"/libs/src/assets/icons/SvgMinus.svg":H,"/libs/src/assets/icons/SvgMoreVert.svg":B,"/libs/src/assets/icons/SvgNavigateArrow.svg":T,"/libs/src/assets/icons/SvgNotification.svg":O,"/libs/src/assets/icons/SvgOffline.svg":$,"/libs/src/assets/icons/SvgOnline.svg":F,"/libs/src/assets/icons/SvgPlus.svg":N,"/libs/src/assets/icons/SvgPower.svg":L,"/libs/src/assets/icons/SvgQuestionCircle.svg":D,"/libs/src/assets/icons/SvgSearch.svg":A,"/libs/src/assets/icons/SvgSettings.svg":k,"/libs/src/assets/icons/SvgSuccessCircle.svg":x,"/libs/src/assets/icons/SvgTemperature.svg":M,"/libs/src/assets/icons/SvgUser.svg":y,"/libs/src/assets/icons/SvgVisibility.svg":w,"/libs/src/assets/icons/SvgVisibilityOff.svg":C,"/libs/src/assets/icons/SvgWarning.svg":I,"/libs/src/assets/icons/SvgWarningTri.svg":f,"/libs/src/assets/icons/SvgWindow.svg":S}),r=Object.keys(ws).map(s=>s.replace(/^.*[\\/]/,"").replace(/\.svg$/,"")).filter(s=>s!=="SvgAuo"),ys=d(null),t=d(null);function Ms(s){navigator.clipboard.writeText(s).then(()=>{t.value=s,setTimeout(()=>t.value=null,1500)}).catch(o=>console.error("複製失敗:",o))}const Ds={title:"Component/Icon",component:a,tags:["autodocs"],argTypes:{name:{description:"Svg名稱",control:{type:"select"},options:r},size:{description:"尺寸(px)",control:{type:"text"}},color:{description:"顏色",control:{type:"color"},table:{type:{summary:"#000000 | black"}}}},parameters:{docs:{title:"Icon",description:{component:"圖標組件的呈現及說明。"}}}},c={name:"預設項目",args:{name:"SvgHome",size:"48",color:"#0D64FB"},render:s=>({components:{Icon:a},setup(){return{args:s,icons:r}},template:`
+      <Icon
+        :name="args.name"
+        :size="args.size"
+        :color="args.color"
+      ></Icon>
+        `}),parameters:{controls:{exclude:["width","height","src"]},docs:{source:{transform:(s,o)=>{const{args:n}=o;return["<script setup>",'import { Icon } from "@ded-wds-vue/ui";',"<\/script>","","<template>","  <Icon",`    ${n.name?`name="${n.name}"`:""}`,`    ${n.size?`size="${n.size}"`:""}`,`    ${n.color?`color="${n.color}"`:""}`,"  >","  </Icon>","</template>"].join(`
+`).trim()}}}}},i={name:"圖標列表",args:{size:"48",color:"#0D64FB"},render:s=>({components:{Icon:a},setup(){return{args:s,icons:r,hoveredIcon:ys,copiedIcon:t,copyToClipboard:Ms}},template:`
+      <div class="flex flex-wrap gap-4">
+        <div
+          v-for="iconName in icons"
+          @click="copyToClipboard(iconName)"
+          @mouseover="hoveredIcon = iconName"
+          @mouseleave="hoveredIcon = null"
+          class="icon-button relative cursor-pointer"
+        >
+          <Icon
+            :name="iconName"
+            :size="args.size"
+            :color="args.color"
+          ></Icon>
+          <span class="text-xs">{{ iconName }}</span>
+
+          <!-- Hover 時顯示 Click -->
+          <span
+            v-if="hoveredIcon === iconName && copiedIcon !== iconName"
+            class="icon-button__tooltip"
+          >
+      Click
+    </span>
+
+          <!-- 點擊後顯示 Copied! -->
+          <span
+            v-if="copiedIcon === iconName"
+            class="icon-button__tooltip"
+          >
+      Copied!
+    </span>
+        </div>
+      </div>
+    `}),parameters:{controls:{exclude:["width","height","src"]},docs:{source:{code:!1}}}};var l,_,v;c.parameters={...c.parameters,docs:{...(l=c.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: "預設項目",
+  args: {
+    name: "SvgHome",
+    size: "48",
+    color: "#0D64FB"
+  },
+  render: args => ({
+    components: {
+      Icon
+    },
+    setup() {
+      return {
+        args,
+        icons
+      };
+    },
+    template: \`
+      <Icon
+        :name="args.name"
+        :size="args.size"
+        :color="args.color"
+      ></Icon>
+        \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      exclude: ["width", "height", "src"]
+    },
+    docs: {
+      source: {
+        transform: (src, storyContext) => {
+          const {
+            args
+          } = storyContext;
+          return [\`<script setup>\`, \`import { Icon } from "@ded-wds-vue/ui";\`, \`<\/script>\`, "", "<template>", "  <Icon", \`    \${args.name ? \`name="\${args.name}"\` : ""}\`, \`    \${args.size ? \`size="\${args.size}"\` : ""}\`, \`    \${args.color ? \`color="\${args.color}"\` : ""}\`, "  >", "  </Icon>", "</template>"].join("\\n").trim();
+        }
+      }
+    }
+  }
+}`,...(v=(_=c.parameters)==null?void 0:_.docs)==null?void 0:v.source}}};var g,m,b;i.parameters={...i.parameters,docs:{...(g=i.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  name: "圖標列表",
+  args: {
+    // name: "SvgHome",
+    size: "48",
+    color: "#0D64FB"
+  },
+  render: args => ({
+    components: {
+      Icon
+    },
+    setup() {
+      return {
+        args,
+        icons,
+        hoveredIcon,
+        copiedIcon,
+        copyToClipboard
+      };
+    },
+    template: \`
+      <div class="flex flex-wrap gap-4">
+        <div
+          v-for="iconName in icons"
+          @click="copyToClipboard(iconName)"
+          @mouseover="hoveredIcon = iconName"
+          @mouseleave="hoveredIcon = null"
+          class="icon-button relative cursor-pointer"
+        >
+          <Icon
+            :name="iconName"
+            :size="args.size"
+            :color="args.color"
+          ></Icon>
+          <span class="text-xs">{{ iconName }}</span>
+
+          <!-- Hover 時顯示 Click -->
+          <span
+            v-if="hoveredIcon === iconName && copiedIcon !== iconName"
+            class="icon-button__tooltip"
+          >
+      Click
+    </span>
+
+          <!-- 點擊後顯示 Copied! -->
+          <span
+            v-if="copiedIcon === iconName"
+            class="icon-button__tooltip"
+          >
+      Copied!
+    </span>
+        </div>
+      </div>
+    \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      exclude: ["width", "height", "src"]
+    },
+    docs: {
+      source: {
+        code: false
+      }
+    }
+  }
+}`,...(b=(m=i.parameters)==null?void 0:m.docs)==null?void 0:b.source}}};const Ls=["IconDefaultStory","IconListStory"];export{c as IconDefaultStory,i as IconListStory,Ls as __namedExportsOrder,Ds as default};
---
+++ assets/List.stories-DuFs0l9x.js
@@ -0,0 +1,172 @@
+import{_ as o}from"./List-Cy1ohMpt.js";import{a as i}from"./index-B-lxVbXh.js";import"./vue.esm-bundler-CC1hEtM-.js";import"./Icon-DU5IBFbg.js";import"./v4-CtRu48qb.js";function c(t){return`    ${t.map(a=>`{
+        ${[a.label?`label: "${a.label}"`:"",a.value?`value: "${a.value}"`:"",a.href?`href: "${a.href}"`:"",a.prefix?`prefix: "${a.prefix}"`:"",a.isDisabled!==void 0?`isDisabled: ${a.isDisabled}`:""].filter(Boolean).join(`,
+        `)}
+    }`).join(`,
+    `)}`}const l=[{label:"Option1",value:"option1",prefix:"SvgAccount",isDisabled:!0},{label:"Option2",value:"option2",href:"#",prefix:"SvgAccount"},{label:"Option3",value:"option3",href:"#",prefix:"SvgAccount"}],k={title:"Component/List",component:o,tags:["autodocs"],argTypes:{dataSource:{description:"資料來源",control:{type:"object"},table:{type:{summary:"{ label: string, value: string, href: string, prefix: string, isDisabled: boolean }[]"}}},hasOutline:{description:"是否為選單",control:{type:"boolean"}},hasDivider:{description:"是否有分隔線",control:{type:"boolean"}},className:{description:"客製化樣式",control:{type:"text"}},onSelect:{description:"選擇項目時觸發的事件",control:!1,table:{category:"EVENTS",type:{summary:"emit"},defaultValue:{summary:()=>({})}}}},parameters:{docs:{title:"List",description:{component:"清單組件的呈現及說明。"}}}},s={name:"預設項目",args:{dataSource:l,hasOutline:!1,hasDivider:!1,className:""},render:t=>({components:{List:o},setup(){return{args:t,handleItemClick:i("onSelect")}},template:`
+      <List
+        :dataSource="args.dataSource"
+        :hasOutline="args.hasOutline"
+        :hasDivider="args.hasDivider"
+        :className="args.className"
+        @onSelect="handleItemClick"
+      >
+      </List>
+    `}),parameters:{controls:{exclude:["default"]},docs:{source:{transform:(t,a)=>{const{args:e}=a;return["<script setup>",'import { List } from "@ded-wds-vue/ui";',"const dataSource = [",`${c(e.dataSource)}`,"];","const handleItemClick = () => {};","<\/script>","","<template>","  <List",'    :dataSource="dataSource"',`    ${e.hasOutline!==void 0?`:hasOutline="${e.hasOutline}"`:""}`,`    ${e.hasDivider!==void 0?`:hasDivider="${e.hasDivider}"`:""}`,`    ${e.className?`className="${e.className}"`:""}`,'    @onSelect="handleItemClick()"',"  ></List>","</template>"].filter(Boolean).join(`
+`).trim()}}}}},n={name:"清單樣式-外框",args:{dataSource:l,hasDivider:!1,className:""},render:t=>({components:{List:o},setup(){return{args:t,handleItemClick:i("onSelect")}},template:`
+      <List
+        :dataSource="args.dataSource"
+        :hasOutline="true"
+        :hasDivider="args.hasDivider"
+        :className="args.className"
+        @onSelect="handleItemClick"
+      >
+      </List>
+    `}),parameters:{controls:{exclude:["hasOutline","default"]},docs:{source:{transform:(t,a)=>{const{args:e}=a;return["<script setup>",'import { List } from "@ded-wds-vue/ui";',"const dataSource = [",`${c(e.dataSource)}`,"];","const handleItemClick = () => {};","<\/script>","","<template>","  <List",'    :dataSource="dataSource"','    :hasOutline="true"',`    ${e.hasDivider!==void 0?`:hasDivider="${e.hasDivider}"`:""}`,`    ${e.className?`className="${e.className}"`:""}`,'    @onSelect="handleItemClick()"',"  ></List>","</template>"].filter(Boolean).join(`
+`).trim()}}}}},r={name:"清單樣式-分隔線",args:{dataSource:l,hasOutline:!1,className:""},render:t=>({components:{List:o},setup(){return{args:t,handleItemClick:i("onSelect")}},template:`
+      <List
+        :dataSource="args.dataSource"
+        :hasOutline="args.hasOutline"
+        :hasDivider="true"
+        :className="args.className"
+        @onSelect="handleItemClick"
+      >
+      </List>
+    `}),parameters:{controls:{exclude:["hasDivider","default"]},docs:{source:{transform:(t,a)=>{const{args:e}=a;return["<script setup>",'import { List } from "@ded-wds-vue/ui";',"const dataSource = [",`${c(e.dataSource)}`,"];","const handleItemClick = () => {};","<\/script>","","<template>","  <List",'    :dataSource="dataSource"',`    ${e.hasOutline!==void 0?`:hasOutline="${e.hasOutline}"`:""}`,'    :hasDivider="true"',`    ${e.className?`className="${e.className}"`:""}`,'    @onSelect="handleItemClick()"',"  ></List>","</template>"].filter(Boolean).join(`
+`).trim()}}}}};var d,m,p;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  name: "預設項目",
+  args: {
+    dataSource,
+    hasOutline: false,
+    hasDivider: false,
+    className: ""
+  },
+  render: args => ({
+    components: {
+      List
+    },
+    setup() {
+      return {
+        args,
+        handleItemClick: action("onSelect")
+      };
+    },
+    template: \`
+      <List
+        :dataSource="args.dataSource"
+        :hasOutline="args.hasOutline"
+        :hasDivider="args.hasDivider"
+        :className="args.className"
+        @onSelect="handleItemClick"
+      >
+      </List>
+    \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      exclude: ["default"]
+    },
+    docs: {
+      source: {
+        transform: (src, storyContext) => {
+          const {
+            args
+          } = storyContext;
+          const dataSourceString = formatDataSource(args.dataSource);
+          return [\`<script setup>\`, \`import { List } from "@ded-wds-vue/ui";\`, \`const dataSource = [\`, \`\${dataSourceString}\`, \`];\`, \`const handleItemClick = () => {};\`, \`<\/script>\`, "", "<template>", \`  <List\`, \`    :dataSource="dataSource"\`, \`    \${args.hasOutline !== undefined ? \`:hasOutline="\${args.hasOutline}"\` : ""}\`, \`    \${args.hasDivider !== undefined ? \`:hasDivider="\${args.hasDivider}"\` : ""}\`, \`    \${args.className ? \`className="\${args.className}"\` : ""}\`, \`    @onSelect="handleItemClick()"\`, \`  ></List>\`, "</template>"].filter(Boolean).join("\\n").trim();
+        }
+      }
+    }
+  }
+}`,...(p=(m=s.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var S,h,g;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  name: "清單樣式-外框",
+  args: {
+    dataSource,
+    hasDivider: false,
+    className: ""
+  },
+  render: args => ({
+    components: {
+      List
+    },
+    setup() {
+      return {
+        args,
+        handleItemClick: action("onSelect")
+      };
+    },
+    template: \`
+      <List
+        :dataSource="args.dataSource"
+        :hasOutline="true"
+        :hasDivider="args.hasDivider"
+        :className="args.className"
+        @onSelect="handleItemClick"
+      >
+      </List>
+    \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      exclude: ["hasOutline", "default"]
+    },
+    docs: {
+      source: {
+        transform: (src, storyContext) => {
+          const {
+            args
+          } = storyContext;
+          const dataSourceString = formatDataSource(args.dataSource);
+          return [\`<script setup>\`, \`import { List } from "@ded-wds-vue/ui";\`, \`const dataSource = [\`, \`\${dataSourceString}\`, \`];\`, \`const handleItemClick = () => {};\`, \`<\/script>\`, "", "<template>", \`  <List\`, \`    :dataSource="dataSource"\`, \`    :hasOutline="true"\`, \`    \${args.hasDivider !== undefined ? \`:hasDivider="\${args.hasDivider}"\` : ""}\`, \`    \${args.className ? \`className="\${args.className}"\` : ""}\`, \`    @onSelect="handleItemClick()"\`, \`  ></List>\`, "</template>"].filter(Boolean).join("\\n").trim();
+        }
+      }
+    }
+  }
+}`,...(g=(h=n.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var f,D,v;r.parameters={...r.parameters,docs:{...(f=r.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  name: "清單樣式-分隔線",
+  args: {
+    dataSource,
+    hasOutline: false,
+    // hasDivider: false,
+    className: ""
+  },
+  render: args => ({
+    components: {
+      List
+    },
+    setup() {
+      return {
+        args,
+        handleItemClick: action("onSelect")
+      };
+    },
+    template: \`
+      <List
+        :dataSource="args.dataSource"
+        :hasOutline="args.hasOutline"
+        :hasDivider="true"
+        :className="args.className"
+        @onSelect="handleItemClick"
+      >
+      </List>
+    \`
+  }),
+  // 控制 controls 中能控制的參數
+  parameters: {
+    controls: {
+      exclude: ["hasDivider", "default"]
+    },
+    docs: {
+      source: {
+        transform: (src, storyContext) => {
+          const {
+            args
+          } = storyContext;
+          const dataSourceString = formatDataSource(args.dataSource);
+          return [\`<script setup>\`, \`import { List } from "@ded-wds-vue/ui";\`, \`const dataSource = [\`, \`\${dataSourceString}\`, \`];\`, \`const handleItemClick = () => {};\`, \`<\/script>\`, "", "<template>", \`  <List\`, \`    :dataSource="dataSource"\`, \`    \${args.hasOutline !== undefined ? \`:hasOutline="\${args.hasOutline}"\` : ""}\`, \`    :hasDivider="true"\`, \`    \${args.className ? \`className="\${args.className}"\` : ""}\`, \`    @onSelect="handleItemClick()"\`, \`  ></List>\`, "</template>"].filter(Boolean).join("\\n").trim();
+        }
+      }
+    }
+  }
+}`,...(v=(D=r.parameters)==null?void 0:D.docs)==null?void 0:v.source}}};const I=["ListDefaultStory","ListOutLineStory","ListDividerStory"];export{s as ListDefaultStory,r as ListDividerStory,n as ListOutLineStory,I as __namedExportsOrder,k as default};
