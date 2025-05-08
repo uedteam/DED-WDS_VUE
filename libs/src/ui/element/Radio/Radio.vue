8c27ff27efe884366d96afa86fb497e68e0151f3
Kevin <kk2229.yang@gmail.com>
2025-05-08 10:44:39 +0800

    feat(radio): 新增 Radio 元件測試範例
    新增 Radio 元件的測試範例，包含各種屬性設定及控制功能，並在掛載時顯示初始選擇值。

--- libs/src/ui/element/Radio/Radio.vue
+++ libs/src/ui/element/Radio/Radio.vue
@@ -1,8 +1,8 @@
 <script setup>
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from 'uuid';
 
 // 阻止 Vue 自動將 $attrs 綁定到最外層 <div>
-defineOptions({ inheritAttrs: false })
+defineOptions({ inheritAttrs: false });
 // 定義 Props
 const props = defineProps({
   dataSource: {
@@ -11,30 +11,31 @@ const props = defineProps({
   },
   direction: {
     type: String,
-    default: "row",
-    validator: value => ["row", "column"].includes(value),
+    default: 'row',
+    validator: (value) => ['row', 'column'].includes(value),
   },
   size: {
     type: String,
-    default: "medium",
-    validator: value => ["small", "medium", "large"].includes(value),
+    default: 'medium',
+    validator: (value) => ['small', 'medium', 'large'].includes(value),
   },
   className: {
     type: String,
-    default: "",
+    default: '',
   },
-})
+});
 // 使用唯一識別碼生成方法
-const baseId = uuidv4()
-const generateId = index => `${baseId}-radio-${index}`
+const baseId = uuidv4();
+const generateId = (index) => `${baseId}-radio-${index}`;
 
 // 定義 Model
-const modelValue = defineModel()
+const modelValue = defineModel();
 </script>
 
 <template>
   <div
-    class="ded-radio-container" :class="{
+    class="ded-radio-container"
+    :class="{
       [`ded-radio-container-${props.direction}`]: props.direction,
       [props.className]: !!props.className,
     }"
@@ -57,9 +58,11 @@ const modelValue = defineModel()
         type="radio"
         :value="item.value"
         :name="item.name"
-      >
+        :disabled="item.isDisabled"
+      />
       <div
-        class="ded-radio-icon" :class="{
+        class="ded-radio-icon"
+        :class="{
           [`ded-icon-${props.size}`]: props.size,
           'ded-radio-icon-disabled': item.isDisabled,
           'ded-radio-checked': modelValue === item.value,
@@ -76,6 +79,4 @@ const modelValue = defineModel()
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
